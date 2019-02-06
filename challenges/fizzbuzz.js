ad4b04b376c7de949ee6b4bad5445963527f5873
yudataguy <yudataguy@gmail.com>
2019-02-06 09:27:43 -0800

    finished fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,30 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {
+// function fizzbuzz(num) {
+//   let output = []
+//   for (let i = 1; i < num; i++){
+//     if (i % 3 === 0 && i % 5 === 0){
+//       output.push('fizzbuzz')
+//     } else if (i % 3 === 0) {
+//       output.push('fizz')
+//     } else if (i % 5 === 0) {
+//       output.push('buzz')
+//     } else {
+//       output.push(i)
+//     }
+//   }
+//   return output
+// }
 
+function fizzbuzz(num) { // shorthand ver
+  let output = []
+  for(let i = 1; i < num; i++){
+    i % 3 === 0 && i % 5 === 0 ? output.push('fizzbuzz') : i % 3 === 0 ? output.push('fizz') : i % 5 === 0 ? output.push('buzz') : output.push(i)
+  }
+  return output
 }
+
+// console.log(fizzbuzz(30))
 
 module.exports = fizzbuzz;

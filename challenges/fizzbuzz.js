e044c0cec60be371b2660d7d0b3519e456d7fcb1
Ray Yao <rocaray51@gmail.com>
2019-02-06 09:18:17 -0800

    Update fizzbuzz.js

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,24 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  //Guard clauses
+	if (typeof num !== 'number') return 'Please enter numbers only';
+  if (num % Math.floor(num) !== 0) return 'Whole numbers only';
 
+  //create output array
+  let output = [];
+  //iterate 1 to num times
+  for (let i = 1; i <= num; i ++){
+  	//if n % 15 is 0, push "fizzbuzz"
+  	if (i % 15 == 0 ) output.push("fizzbuzz");
+    //if n % 3 is 0, push "fizz"
+    else if (i % 5 == 0) output.push("buzz");
+    //if n % 5 is 0, push "buzz"
+    else if (i % 3 == 0) output.push("fizz");
+    //all other numbers, push that number
+    else output.push(i);
+  }
+  return output;
 }
 
 module.exports = fizzbuzz;

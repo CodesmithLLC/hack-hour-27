a849a9910bffacbd7f0713f82d93915659ce0488
Quoc Bui <quocbui@gmail.com>
2019-02-06 09:19:04 -0800

    finished hack hour

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -1,4 +1,7 @@
-// Write a function that returns an array containing the numbers 1 to NUM. Put "fizz" in place of numbers divisble by 3, "buzz" in place of numbers divisble by 5, and "fizzbuzz" in place of numbers divisble by both 3 and 5
+// Write a function that returns an array containing the numbers 1 to NUM. 
+// Put "fizz" in place of numbers divisble by 3, 
+// "buzz" in place of numbers divisble by 5, and 
+// "fizzbuzz" in place of numbers divisble by both 3 and 5
 // fizzbuzz(16);  -> [ 1,
 //                     2,
 //                     'fizz',
@@ -17,7 +20,36 @@
 //                     16 ]
 
 function fizzbuzz(num) {
+  let output = [];
 
+  function divBy3(num) {
+    return num % 3 === 0;
+  }
+
+  function divBy5(num) {
+    return num % 5 === 0;
+  }
+
+  for (let i = 1; i <= num; i++) {
+    if (divBy3(i) && divBy5(i)) {
+      output.push('fizzbuzz');
+    } else if (divBy3(i)) {
+      output.push('fizz');
+    } else if (divBy5(i)) {
+      output.push('buzz');
+    } else {
+      output.push(i);
+    }
+  }
+
+  return output;
 }
+
+console.log('0 - ' + fizzbuzz(0));
+console.log('5 - ' + fizzbuzz(5));
+console.log('10 - ' + fizzbuzz(10));
+console.log('20 - ' + fizzbuzz(20));
+
+
 
 module.exports = fizzbuzz;

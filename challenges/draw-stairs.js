89aaa72895ba70710fd088ab7c0ccdcd4f972ea3
Sterling Deng <sterlingdeng@gmail.com>
2019-02-08 09:43:37 -0800

    completed function drawStairs for inputs from 1 to 100

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,20 @@
 */
 
 function drawStairs(n) {
-
+  if (n < 1 || n > 100) return "number out of range";
+  let strStairs = "";
+  for (let i = 0; i < n; i += 1) {
+    for (let j = 0; j < n - i - 1; j += 1) {
+      strStairs += " ";
+    }
+    for (let k = 0; k < i + 1; k += 1) {
+      strStairs += "*";
+    }
+    strStairs += "\n";
+  }
+  console.log(strStairs);
 }
 
+// drawStairs(30);
 
 module.exports = drawStairs;

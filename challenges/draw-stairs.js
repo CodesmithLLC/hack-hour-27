4020882589d89689d973c7ad44e1737db7633772
p <petulacode@gmail.com>
2019-02-08 09:57:46 -0800

    finished draw stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,29 @@
 */
 
 function drawStairs(n) {
+//input will be a number value of n that will determine the numnber of rows it will print
+//on first row will print just one asterisk
+//on additional row will add one asterisk to the left 
 
+//strategy: create an array that will hold n number of blank spaces
+  //loop through array starting from the end of the array, push an * to array on each iteration
+  //join the array and print row on each loop
+
+  
+  let row = []; 
+  for (let i = 0; i < n; i++) {
+    {
+    row.push(' '); 
+    }
+  }
+  
+  for (let i = row.length-1; i >= 0 && i < 100; i--) { 
+    row[i] = '*'; 
+    console.log(row.join('')); 
+  } 
 }
 
+console.log(drawStairs(4));
+console.log(drawStairs(6));
 
 module.exports = drawStairs;

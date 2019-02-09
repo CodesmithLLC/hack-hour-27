568833d3c6716b7e3401553798d374fb30a042d4
Parker Allen <parkerjallen1@gmail.com>
2019-02-09 09:36:29 -0800

    flex on the hackers

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -12,6 +12,19 @@
 
 function modemean(array) {
 
+    const mean = Math.floor(array.reduce((a, b) => a + b) / array.length);
+    let tempMode = [array[0], 1]
+    const mode = array.reduce((acc, val) => {
+        if (acc[val] === undefined) {
+            acc[val] = 1;
+        } else {
+            acc[val] += 1
+        }
+        if (acc[val] > tempMode[1]) tempMode = [val, acc[val]];
+        return acc;
+    }, {})
+    
+    return mean === tempMode[0];
 }
 
 module.exports = modemean;

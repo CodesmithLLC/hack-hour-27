4aff16677cc84782c9f4214c659d0f5c0b4440be
Chris Air <sdchrisn@gmail.com>
2019-02-09 09:27:50 -0800

    mode-mean completed and tested

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,23 @@
 
 
 function modemean(array) {
+    let mean = Math.floor(array.reduce((acc, curr) => acc + curr) / array.length);
+    // console.log('mean', mean);
 
+    let mode = {};
+    array.forEach((elem, i) =>{
+        mode[elem] = (mode[elem]) ? 1 : mode[elem]++;
+    });
+    let maxkey = Object.keys(array).reduce((maxkey, nextkey) => {
+        if(mode[maxkey] > mode[nextkey] )
+            return maxkey;
+        return nextkey;
+    });
+    // console.log('key', maxkey, 'value', array[maxkey]);
+    if(mean === array[maxkey])
+        return true;
+    return false;
 }
+// console.log(modemean([2,2,2,7]));
 
 module.exports = modemean;

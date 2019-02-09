355ee85d36e561e02683b81a1907a4f223f6494b
tang <eytang8@gmail.com>
2019-02-09 09:30:16 -0800

    finished modemean. added tests

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,46 @@
 
 
 function modemean(array) {
-
+  return (mode(array) === mean(array));
 }
 
+function mode(arr){
+  //create and maintain count of all elements
+  const counts = {};
+  arr.forEach((item)=>{
+    if (!counts[item]){
+      counts[item] = 0;
+    }
+    counts[item]++;
+  })
+
+  // get an initial mode;
+  let mode = arr.reduce((a,b)=>{
+      if (counts[a] > counts[b]){
+        return a;
+      }
+      return b;
+    })
+  
+  // checks if any of the other possible modes are greater;
+  arr.forEach((item)=>{
+    if (counts[item] === counts[mode]){
+      if (item > mode) mode = item;
+    }
+  })
+  return mode;
+  
+}
+/* test for mode:
+const testMode = [0, 2, 5, 3, 3, 3, 7, 7, 7, 24 ];
+console.log('mode. expect: 7, got: ' + mode(testMode));
+*/
+
+function mean(arr){
+  return Math.floor(arr.reduce((a,b)=>{return a+b})/arr.length);
+}
+/* test for mean:
+const testMean = [2, 4, 6, 8];
+console.log('mean. expect: 5, got: ' + mean(testMean));
+*/
 module.exports = modemean;

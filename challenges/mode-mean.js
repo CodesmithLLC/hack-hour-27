2ee6d7a289dfc69cde07de3ff4a9e77f8feeaa94
mlu456 <michaellu213@gmail.com>
2019-02-09 09:49:35 -0800

    mode mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,29 @@
 
 
 function modemean(array) {
-
+  let hash = {};
+  let sum = 0;
+  let maxKey = [];
+  for (let i = 0; i < array.length; i++) {
+    // find mode
+    if (hash[array[i]] === undefined) {
+      hash[array[i]] = 0;
+    }
+    hash[array[i]] += 1;
+    // find sum
+    sum += array[i];
+  }
+  let keys = Object.keys(hash);
+  let highestRecur = hash[keys[0]];
+  for (let i = 0; i < keys.length; i++) {
+    if (hash[keys[i]] >= highestRecur) {
+      highestRecur = hash[keys[i]];
+      maxKey.push(keys[i]);
+    }
+  }
+  return Math.floor(sum/array.length) === Math.max(...maxKey);
 }
 
 module.exports = modemean;
+
+modemean([0, 1, 1, 2, 2, 1, 0, 0, 0, 1])

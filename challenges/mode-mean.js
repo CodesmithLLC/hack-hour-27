824e91fa974ca5e6d8de501dce278a0920e889d8
Shane Taylor <shaneallantaylor@gmail.com>
2019-02-09 09:47:10 -0800

    mode-mean working will all my tests

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,6 +11,35 @@
 
 
 function modemean(array) {
+  // calculate the mode
+  // the most frequently occuring number in the array
+  // use an object to calculate the mode
+  const modeObj = {};
+  let mean = null;
+  for (let i = 0; i < array.length; i++) {
+    mean += array[i];
+    if (!modeObj[array[i]]) {
+      modeObj[array[i]] = 1;
+    } else {
+      modeObj[array[i]] += 1;
+    }
+  }
+  // calculate the mean
+  // when calc'ing, use math.floor
+  mean = Math.floor(mean / array.length);
+  let mode = null;
+  let curMax = null;
+  for (let key in modeObj) {
+    if (modeObj[key] >= curMax) {
+      curMax = modeObj[key];
+      if (key > mode) {
+        mode = key;
+      }
+    }
+  }
+  // compare
+  // return true or false
+  return mode == mean;
 
 }
 

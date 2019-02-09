247cd37ce95120060b98f6b4758d0fc923d5f876
Augustine Kim <ajk830@gmail.com>
2019-02-09 09:29:56 -0800

    mode-mean first attempt

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,47 @@
 
 
 function modemean(array) {
+  // storages
+  let sum = 0;
+  let store = {};
+  let mode;
 
+  // go through values in array
+  array.forEach((value)=>{
+    // tally sum
+    sum += value;
+
+    // keep track of how many of each number we see
+    if (store[value] === undefined) {
+      store[value] = 1;
+    } else {
+      store[value]++;
+    }
+
+    // check if mode should be updated
+    if (mode === undefined) {
+      mode = value;
+    } else if (store[value] === store[mode]) {
+      if (value > mode) {
+        mode = value;
+      }
+    } else if (store[value] > store[mode]) {
+      mode = value;
+    }
+
+  });
+
+  return Math.floor(sum/array.length) === mode;
 }
+
+// const arr1 = [1, 2, 2, 5, 7, 9]; // mode: 2, mean: 4.33
+// const arr2 = [1, 3, 4, 4, 8]; // mode: 4, mean: 4
+// const arr3 = [3, 5, 7, 7, 9, 12]; // mode: 7, mean: 7.17
+// const arr4 = [2, 3, 3, 6, 6, 20]; // mode: 6, mean: 6.67
+
+// console.log(modemean(arr1)); // false
+// console.log(modemean(arr2)); // true
+// console.log(modemean(arr3)); // true
+// console.log(modemean(arr4)); // true
 
 module.exports = modemean;

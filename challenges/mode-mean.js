e58a502a0dbbacb1a6962d853ad811907e7a100b
yudataguy <yudataguy@gmail.com>
2019-02-09 09:34:22 -0800

    mode-mean hack hour

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,26 @@
 
 
 function modemean(array) {
+  const mean = Math.floor(array.reduce((acc, cur) => acc + cur) / array.length)
+  const result = {}
+  for (let i = 0; i < array.length; i++){
+    if (!result[array[i]]) {
+      result[array[i]] = 1
+    } else {
+      result[array[i]]++
+    }
+  }
+  const mode = Number(Object.keys(result)[Object.values(result).indexOf(Math.max(Object.values(result)))])
 
+  
+  if (mean === mode) {
+    return true
+  } else {
+    return false
+  }
 }
 
+console.log(modemean([1,2,3]))
+console.log(modemean([6,5,3,6]))
+console.log(modemean([1]))
 module.exports = modemean;

f76a7116c458f5ee69961d2c44bd537336e30fa2
Parker Allen <parkerjallen1@gmail.com>
2019-02-12 09:32:53 -0800

    kth to last index complete YEET

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,28 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let total = 1;
+  let current = head;
+  //iterate over list to find total nodes
+  while (current.next) {
+    total++;
+    current = current.next;
+  }
+  //edge cases
+  if (k > total) {console.log(`there are only ${total} nodes in the list, try a different k`)}
+  if (k === 0) {return "if you are looking for the last node, set k to 1"}
 
+  //set the target index we are looking for to the total of nodes minus the kth from last
+  let target = total - k;
+  let i = 0;
+  let kthNode = head;
+
+  //traverse through the list until we are at the target node
+  while (i < target) {
+    kthNode = kthNode.next;
+    i++;
+  }
+  return kthNode.value;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};

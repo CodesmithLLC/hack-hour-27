43074059511b4abb230d67e1e686eb72efcc0ac0
Chris Air <sdchrisn@gmail.com>
2019-02-12 09:42:40 -0800

    Tested against example only and hoping it passes the rest lol

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,39 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  if(head === null)
+    return;
 
+  let ptr = head;
+  let size = 0;
+  while(ptr !== null){ 
+    size++;
+    ptr = ptr.next;
+  }
+
+  ptr = head;
+  for(let i = 0; i < size; i++){
+    if(i !== size-k){
+      ptr = ptr.next;
+    }
+    else{
+      return ptr.value;
+    }
+  }
+  return;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+  const a = new Node('A');
+  const b = new Node('B');
+  const c = new Node('C');
+  const d = new Node('D');
+  const e = new Node('E');
+ 
+  a.next = b;
+  b.next = c;
+  c.next = d;
+  d.next = e;
+
+console.log(kthToLastNode(2, a));

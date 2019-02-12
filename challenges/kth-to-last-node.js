dad9de9951466529f76566f4dab1b640a4676fc8
Augustine Kim <ajk830@gmail.com>
2019-02-12 09:22:38 -0800

    kth-to-last first attempt

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,36 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let pointer = head;
+  
+  // loop to count length of list
+  let length = 0;
+  while (pointer !== null) {
+    length++;
+    pointer = pointer.next;
+  }
 
+  // reset pointer to head
+  pointer = head;
+  
+  // move until kth position before end
+  for (let i = 0; i < length-k; i++) {
+    pointer = pointer.next;
+  }
+
+  return pointer.value;
 }
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+
+
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+// console.log(kthToLastNode(2, a)); // -> returns 'D' (the value on the second to last node)

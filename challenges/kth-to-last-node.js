f4991071c6472412fa53989776d01aac6644e788
Sterling Deng <sterlingdeng@gmail.com>
2019-02-12 09:43:15 -0800

    kth linked list

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,63 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
-
+  if (k < 1) return false;
+  const maxLength = size(head);
+  const movement = maxLength - k;
+  let curr = head;
+  if (movement < 0) return false;
+  for (let i = 0; i < movement; i += 1) {
+    curr = curr.next;
+  }
+  return curr.value;
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+function size(head) {
+  counter = 0;
+  curr = head;
+  while (curr) {
+    curr = curr.next;
+    counter += 1;
+  }
+  return counter;
+}
+
+function kthToLastNodeRefactored(k, head) {
+  if (k < 1) return false;
+  let curr = head;
+  let counter = 1;
+  let kthTrailingNode = undefined;
+  while (curr) {
+    if (counter === k) {
+      kthTrailingNode = head;
+    } else if (counter > k) {
+      kthTrailingNode = kthTrailingNode.next;
+    }
+    curr = curr.next;
+    counter += 1;
+  }
+  if (!kthTrailingNode) return false;
+  return kthTrailingNode.value;
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+const e = new Node("E");
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+console.log(kthToLastNode(5, b), "should equal", kthToLastNodeRefactored(5, b));
+console.log(kthToLastNode(3, a), "should equal", kthToLastNodeRefactored(3, a));
+console.log(kthToLastNode(2, a), "should equal", kthToLastNodeRefactored(2, a));
+console.log(
+  kthToLastNode(-1, a),
+  "should equal",
+  kthToLastNodeRefactored(-1, a)
+);
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };

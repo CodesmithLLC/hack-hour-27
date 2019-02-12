6e710f844188a0cb0dba135e4380d51620590e61
Shane Taylor <shaneallantaylor@gmail.com>
2019-02-12 09:54:04 -0800

    kth to the last node - two pointers

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,47 @@ function Node(val) {
   this.next = null;
 }
 
+// const a = new Node('A');
+// const b = new Node('B');
+// const c = new Node('C');
+// const d = new Node('D');
+// const e = new Node('E');
+
+// a.next = b;
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
 function kthToLastNode(k, head) {
+  if (k <= 0) {
+    return "Please provide a number larger than 0";
+  }
+
+  // two pointers
+  let curPointer = head;
+  let lookoutPointer = head;
+  for (let i = 1; i < k; i += 1) {
+    if (lookoutPointer.next === null) {
+      return 'Boo! You gave me a number longer than the list!';
+    }
+    lookoutPointer = lookoutPointer.next;
+  }
+
+  while (lookoutPointer) {
+    if (lookoutPointer.next === null) {
+      return curPointer.value;
+    } else {
+      curPointer = curPointer.next;
+      lookoutPointer = lookoutPointer.next;
+    }
+  }
 
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+console.log(kthToLastNode(2, a));
+console.log(kthToLastNode(3, a));
+console.log(kthToLastNode(1, a));
+console.log(kthToLastNode(7, a));
+console.log(kthToLastNode(-2, a));
+
+module.exports = { Node: Node, kthToLastNode: kthToLastNode };

dd31aab3184744b7067de138b2c3c008b296fa7a
justnoah <noahxman@gmail.com>
2019-02-13 09:38:17 -0800

    Completed; pow.js.

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,22 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, result = base) {
+  if (power < 0) return 'Do it in your head.'
+  if (power === 0) return 1;
+  if (power === 1) return result;
+  result *= base;
+  power -= 1;
+  return pow(base, power, result)
 }
+
+function pow(base, power) {
+  if (power < 0) return 'Use a calculator.'
+  if (power === 0) return 1;
+  if (power === 1) return base;
+  return base * pow(base, power - 1); 
+}
+
+console.log(pow(4, 7));
 
 module.exports = pow;

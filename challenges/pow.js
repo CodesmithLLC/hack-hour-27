21c759e87ca07ff8df55c2c6b4d1e0ff5aac1b5e
mlu456 <michaellu213@gmail.com>
2019-02-13 11:47:41 -0800

    pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,23 @@
  */
 
 function pow(base, power) {
+  let result;
+  if (power === 0) return 1;
 
+  if (power > 0) {
+    if (power === 1) return base;
+    result = base * pow(base, power - 1);
+  }
+  if (power < 0) {
+    power *= -1;
+    result = 1 / base * pow(base, -1 * (power - 1));
+    // result = 1 / result;
+  }
+  return result;
 }
 
 module.exports = pow;
+
+console.log(pow(2, -4));
+
+// console.log(Math.pow(3, -1))

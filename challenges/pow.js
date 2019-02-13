829374ff2f6a9262382c410000f8a571cb65edac
tang <eytang8@gmail.com>
2019-02-13 09:17:46 -0800

    added pow + testing functions

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,24 @@
  */
 
 function pow(base, power) {
-
+  if (power === 0) return 1;
+  return base * pow(base, power-1);
 }
+
+/*
+test values up to base^10
+function testBase(base){
+  for (let i = 0; i < 10; i ++){
+    const expected = base**i;
+    const got = pow(base, i);
+    console.log(`${base}^${i}`);
+    console.log(`expected: ${expected}`);
+    console.log(`got: ${got}`);
+  }
+}
+
+testBase(2);
+testBase(5);
+*/
 
 module.exports = pow;

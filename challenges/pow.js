1b20d84c0b4015336f34967ac13000a1d317524a
yudataguy <yudataguy@gmail.com>
2019-02-13 09:28:49 -0800

    finish pow.js

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,15 @@
  */
 
 function pow(base, power) {
-
+  if (power === 0) {
+    return base
+  }
+  if (power < 0) {
+    return 1 / pow(base, Math.abs(power))
+    }
+  if (power > 0) {
+    return base * pow(base, power - 1)
+  }
 }
 
 module.exports = pow;

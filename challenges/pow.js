00a0b936860d5ff2d9f8807dff74b96ef8fbd20a
Augustine Kim <ajk830@gmail.com>
2019-02-13 09:18:39 -0800

    pow complete

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,13 @@
  */
 
 function pow(base, power) {
+  if (power === 0) return 1;
+  if (power === 1) return base;
 
+  return base * pow(base, power - 1);
 }
 
 module.exports = pow;
+
+
+// console.log(pow(3,3));

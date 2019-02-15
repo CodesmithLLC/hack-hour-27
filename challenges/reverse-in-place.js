88367b1d3e3c84a535dbe789c5163f04491e4557
Sterling Deng <sterlingdeng@gmail.com>
2019-02-15 11:26:59 -0800

    reverse in place

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -1,4 +1,5 @@
-'use strict';
+"use strict";
+// const _ = require("lodash");
 /**
  * Write a function to reverse an array in place
  *
@@ -14,7 +15,38 @@
  */
 
 function reverseInPlace(array) {
-
+  let hi = array.length - 1;
+  for (let lo = 0; lo < hi; lo += 1) {
+    let temp = array[lo];
+    array[lo] = array[hi];
+    array[hi] = temp;
+    hi -= 1;
+  }
+  return array;
 }
+
+// const testPair = [
+//   { input: [], expect: [] },
+//   { input: [1, 2, 3], expect: [3, 2, 1] },
+//   { input: [1, 2, 3, 4], expect: [4, 3, 2, 1] },
+//   { input: ["a", "b", "c"], expect: ["a", "b", "c"] },
+//   { input: [[1], [2], [3]], expect: [[3], [2], [1]] },
+//   { input: [[1], [2], [3]], expect: [[3], [2], [1]] },
+//   { input: [], expect: [] }
+// ];
+
+// testPair.forEach(test => {
+//   const { input, expect } = test;
+//   const output = reverseInPlace(input);
+//   if (_.isEqual(expect, output)) {
+//     console.log(
+//       `test failed, input ${input}, expected ${expect}, got ${output} \n`
+//     );
+//   } else {
+//     console.log(
+//       `test passed, input ${input}, expected ${expect}, got ${output} \n`
+//     );
+//   }
+// });
 
 module.exports = reverseInPlace;

be2d52f46eff03468c2751963162b116fd364b43
mlu456 <michaellu213@gmail.com>
2019-02-16 09:42:07 -0800

    first attempt

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -9,6 +9,13 @@
  *              stringRotation("hello", "llohe") -> true
  *              stringRotation("hello", "he") -> false
  *              stringRotation("hello", "ollhe") -> false (not a rotation, just an anagram)
+ possible rotations:
+  hello
+  elloh
+  llohe
+  lohel
+  ohell
+  hello
  */
 
 function isSubstring(s1, s2) {
@@ -16,7 +23,35 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+  if (s1 === s2) return true;
+  if (s1.length !== s2.length) return false;
 
+  let index1 = 0;
+  let index2 = s2.indexOf(s1[index1]);
+  while (index1 < s1.length - 1) {
+    index1 += 1;
+    index2 += 1;
+    if (index2 > s2.length - 1) {
+      index2 = 0;
+    }
+    if (s1[index1] !== s2[index2]) {
+      return false;
+    }
+  }
+  return true;
 }
 
+// console.log(stringRotation('hello', 'elloh'));
+// console.log(stringRotation('hello', 'llohe'));
+// console.log(stringRotation('hello', 'lohel'));
+// console.log(stringRotation('hello', 'ohell'));
+// console.log(stringRotation("hello", "he"));
+console.log(stringRotation("lol", "oll"));
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
+
+
+// stringRotation('hello', 'ohell')
+// stringRotation('hello', 'hello')
+
+
+// console.log(isSubstring('hello', ''))

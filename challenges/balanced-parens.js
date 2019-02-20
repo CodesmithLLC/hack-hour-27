df3e2e4047b33ddb0216801ddef6671e11dd1635
Ed <15ryane@gmail.com>
2019-02-20 10:00:50 -0800

    this is not very good and messy

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -26,6 +26,55 @@
 
 function balancedParens(input){
 
+	//recurseeeeee
+
+ 	let newInput = input;
+
+ 	let openParens = 1;
+	let closedParens = 0;
+
+	let start = input.indexOf('(')
+	let end = input.indexOf(')')
+
+	// console.log(start)
+	// console.log(end)
+
+	// base cases
+	if( (start === -1) && (end === -1) && (start>=end) ) return true
+	if( (start > end) ) return false
+	if  (start === -1) return false;
+	if  (end === -1) return false;
+
+
+	// recu case
+	let i = start+1;
+
+	do{
+		if(input[i] === ')') {
+			closedParens++;
+			end = i;
+		}
+
+		if(input[i] === '(') openParens++;
+		i++;
+
+	} while( (closedParens!==openParens) || i<input.length)
+
+	// remove the parens
+	newInput = Array.from(newInput)
+	newInput[start] = '0'
+	newInput[end] = '0'
+	newInput = newInput.join('')
+
+	return balancedParens(newInput)
 }
+
+// tests
+
+console.log(balancedParens('('));  // false
+console.log(balancedParens('()')); // true
+console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
+console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
 
 module.exports = balancedParens;

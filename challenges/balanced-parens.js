991a3231227b29b3d4f4c6f3766c32e8a0ee1c51
dasbus <90valentin@gmail.com>
2019-02-20 10:00:31 -0800

    balanced parents completed

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -26,6 +26,42 @@
 
 function balancedParens(input){
 
+
+arr = input.split('');
+arr2 = [];
+for (let each of arr){
+	if (each.replace(/[^{}()\[\]]/, "")){
+		arr2.push(each);
+	}
 }
+sanitized = arr2.join('');
+
+let i;
+let counter = 0;
+let fin = input.length - 1 ;
+let ledger = {
+	'{': '}',
+	'(': ')',
+	'[': ']'
+};
+
+	for (i=0; i < sanitized.length / 2; i++){
+		for (let [k, v] of Object.entries(ledger)){
+			if ((sanitized[i] === k) && (sanitized[fin - i] === v)) {
+				counter++;
+			}
+		}
+	}
+
+if (counter === sanitized.length / 2) {return true};
+return false;
+
+
+
+}
+console.log(balancedParens('pefj{ffe}'))
+console.log(balancedParens('[{[}]'))
+console.log(balancedParens('[{[]}]'))
+console.log(balancedParens('{}'))
 
 module.exports = balancedParens;

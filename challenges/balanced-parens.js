bca8a49c81f9fa0c7801b32af511e5e9512436d2
Ray Yao <rocaray51@gmail.com>
2019-02-20 15:19:58 -0800

    incomplete

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,46 @@
  */
 
 function balancedParens(input){
-
+  let leftParens = 0;
+  let rightParens = 0;
+  let leftSquare = 0;
+  let rightSquare = 0;
+  let leftCurly = 0;
+  let rightCurly = 0;
+  
+  for (let i = 0; i < input.length; i++){
+  
+  	if (input = '') return true;
+  
+  	//Iterating through string
+  	if (input[i] === '(') leftParens++;
+    if (input[i] === ')') rightParens++;
+    if (input[i] === '[') leftSquare++;
+    if (input[i] === ']') rightSquare++;
+    if (input[i] === '{') leftCurly++;
+    if (input[i] === '}') rightCurly++;
+    
+     //whenever you find a match, remove from input everything starting from the left, until right, and then start iteration again from the new beginning
+    if (leftParens === rightParens && leftParens > 0 && rightParens > 0){
+      let toReplace = input.substr(input.indexOf(leftParens), input.indexOf(rightParens))
+      input = input.replace(toReplace, '')
+    }
+    if (leftSquare === rightSquare && leftSquare > 0 && rightSquare > 0){
+      let toReplace = input.substr(input.indexOf(leftSquare), input.indexOf(rightSquare))
+      input = input.replace(toReplace, '')
+    }
+    if (leftCurly === rightCurly && leftCurly > 0 && rightCurly > 0){
+      let toReplace = input.substr(input.indexOf(leftCurly), input.indexOf(rightCurly))
+      input = input.replace(toReplace, '')
+    }
+    
+    //End of String
+    if (rightParens > leftParens) return false;
+    if (rightSquare > leftSquare) return false;
+    if (rightCurly > leftCurly) return false;
+  }
+  if (leftParens === rightParens && leftSquare === rightSquare && leftCurly === rightCurly) return true;
+  return false;
 }
 
 module.exports = balancedParens;

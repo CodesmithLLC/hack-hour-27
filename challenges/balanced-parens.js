e451681c2d68f0034ebd77b4205bb8be34fbb3f6
Ray Yao <rocaray51@gmail.com>
2019-02-20 19:45:34 -0800

    Update balanced-parens.js

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -12,9 +12,7 @@
  *   make your solution work for all types of brackets
  *
  * Example:
- *  balancedParens('[](){}'); // true
- *  balancedParens('[({})]');   // true
- *  balancedParens('[(]{)}'); // false
+ ````````
  *
  * Step 3:
  * ignore non-bracket characters
@@ -31,10 +29,11 @@ function balancedParens(input){
   let rightSquare = 0;
   let leftCurly = 0;
   let rightCurly = 0;
+  let i;
+  let substringsArray = [];
+  let substring;
   
-  for (let i = 0; i < input.length; i++){
-  
-  	if (input = '') return true;
+for (let i = 0; i < input.length; i++){
   
   	//Iterating through string
   	if (input[i] === '(') leftParens++;
@@ -44,27 +43,51 @@ function balancedParens(input){
     if (input[i] === '{') leftCurly++;
     if (input[i] === '}') rightCurly++;
     
-     //whenever you find a match, remove from input everything starting from the left, until right, and then start iteration again from the new beginning
+   // whenever you find a match, check everything in between the brackets
+    // if everything isnt balanced within this substring, return false;
+    //	else keep going
+    	//return true if reach end of input
+     //whenever you find a match, remove from input everything starting from the /* left, until right, and then start iteration again from the new beginning
     if (leftParens === rightParens && leftParens > 0 && rightParens > 0){
-      let toReplace = input.substr(input.indexOf(leftParens), input.indexOf(rightParens))
-      input = input.replace(toReplace, '')
-    }
+			substringsArray.push(input.slice( input.indexOf('('), input.indexOf(')') + 1))
+      leftParens = 0;
+      rightParens = 0;
+         }
     if (leftSquare === rightSquare && leftSquare > 0 && rightSquare > 0){
-      let toReplace = input.substr(input.indexOf(leftSquare), input.indexOf(rightSquare))
-      input = input.replace(toReplace, '')
-    }
+        substringsArray.push(input.slice( input.indexOf('['), input.indexOf(']') + 1))
+        leftSquare = 0;
+        rightSquare = 0;
+        }
     if (leftCurly === rightCurly && leftCurly > 0 && rightCurly > 0){
-      let toReplace = input.substr(input.indexOf(leftCurly), input.indexOf(rightCurly))
-      input = input.replace(toReplace, '')
-    }
-    
-    //End of String
-    if (rightParens > leftParens) return false;
-    if (rightSquare > leftSquare) return false;
-    if (rightCurly > leftCurly) return false;
+        substringsArray.push(input.slice( input.indexOf('{'), input.indexOf('}') + 1))
+        leftCurly = 0;
+        rightCurly = 0;
+        }
   }
-  if (leftParens === rightParens && leftSquare === rightSquare && leftCurly === rightCurly) return true;
-  return false;
+  console.log('substringsArray', substringsArray)
+  //for each element in substringsArray, iterate thru substring starting from 2nd char until 2nd before last character
+  substringsArray.forEach( (sub) => {
+  	for (let j = 0; j < sub.length; j++){
+      let leftParens = 0;
+      let rightParens = 0;
+      let leftSquare = 0;
+      let rightSquare = 0;
+      let leftCurly = 0;
+      let rightCurly = 0;
+      if (sub[j] === '(') leftParens++;
+      if (sub[j] === ')') rightParens++;
+      if (sub[j] === '[') leftSquare++;
+      if (sub[j] === ']') rightSquare++;
+      if (sub[j] === '{') leftCurly++;
+      if (sub[j] === '}') rightCurly++;
+    }
+    if ( (leftParens === rightParens) && (leftSquare === rightSquare) && (leftCurly === rightCurly) ) return true;
+    else return false;
+  })
 }
+
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // false
 
 module.exports = balancedParens;

2ac886d5a6941bbbbfc438415c045b57e65cfd1e
Ray Yao <rocaray51@gmail.com>
2019-02-21 00:41:16 -0800

    attempt

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -22,7 +22,7 @@
  *
  */
 
-function balancedParens(input){
+function balancedParens(input) {
   let leftParens = 0;
   let rightParens = 0;
   let leftSquare = 0;
@@ -31,63 +31,86 @@ function balancedParens(input){
   let rightCurly = 0;
   let i;
   let substringsArray = [];
-  let substring;
-  
-for (let i = 0; i < input.length; i++){
-  
-  	//Iterating through string
-  	if (input[i] === '(') leftParens++;
-    if (input[i] === ')') rightParens++;
-    if (input[i] === '[') leftSquare++;
-    if (input[i] === ']') rightSquare++;
-    if (input[i] === '{') leftCurly++;
-    if (input[i] === '}') rightCurly++;
-    
-   // whenever you find a match, check everything in between the brackets
+
+  //sanitize string
+  input = input.replace(/([^([{}\]\)])/g, "");
+  //console.log("input", input);
+
+  for (let i = 0; i < input.length; i++) {
+    //Iterating through string
+    if (input[i] === "(") leftParens++;
+    if (input[i] === ")") rightParens++;
+    if (input[i] === "[") leftSquare++;
+    if (input[i] === "]") rightSquare++;
+    if (input[i] === "{") leftCurly++;
+    if (input[i] === "}") rightCurly++;
+
+    // whenever you find a match, check everything in between the brackets
     // if everything isnt balanced within this substring, return false;
     //	else keep going
-    	//return true if reach end of input
-     //whenever you find a match, remove from input everything starting from the /* left, until right, and then start iteration again from the new beginning
-    if (leftParens === rightParens && leftParens > 0 && rightParens > 0){
-			substringsArray.push(input.slice( input.indexOf('('), input.indexOf(')') + 1))
+    //return true if reach end of input
+    //whenever you find a match, remove from input everything starting from the /* left, until right, and then start iteration again from the new beginning
+    if (leftParens === rightParens && leftParens > 0 && rightParens > 0) {
+      substringsArray.push(
+        input.slice(input.indexOf("("), input.indexOf(")") + 1)
+      );
       leftParens = 0;
       rightParens = 0;
-         }
-    if (leftSquare === rightSquare && leftSquare > 0 && rightSquare > 0){
-        substringsArray.push(input.slice( input.indexOf('['), input.indexOf(']') + 1))
-        leftSquare = 0;
-        rightSquare = 0;
-        }
-    if (leftCurly === rightCurly && leftCurly > 0 && rightCurly > 0){
-        substringsArray.push(input.slice( input.indexOf('{'), input.indexOf('}') + 1))
-        leftCurly = 0;
-        rightCurly = 0;
-        }
-  }
-  console.log('substringsArray', substringsArray)
-  //for each element in substringsArray, iterate thru substring starting from 2nd char until 2nd before last character
-  substringsArray.forEach( (sub) => {
-  	for (let j = 0; j < sub.length; j++){
-      let leftParens = 0;
-      let rightParens = 0;
-      let leftSquare = 0;
-      let rightSquare = 0;
-      let leftCurly = 0;
-      let rightCurly = 0;
-      if (sub[j] === '(') leftParens++;
-      if (sub[j] === ')') rightParens++;
-      if (sub[j] === '[') leftSquare++;
-      if (sub[j] === ']') rightSquare++;
-      if (sub[j] === '{') leftCurly++;
-      if (sub[j] === '}') rightCurly++;
     }
-    if ( (leftParens === rightParens) && (leftSquare === rightSquare) && (leftCurly === rightCurly) ) return true;
-    else return false;
-  })
-}
+    if (leftSquare === rightSquare && leftSquare > 0 && rightSquare > 0) {
+      substringsArray.push(
+        input.slice(input.indexOf("["), input.indexOf("]") + 1)
+      );
+      leftSquare = 0;
+      rightSquare = 0;
+    }
+    if (leftCurly === rightCurly && leftCurly > 0 && rightCurly > 0) {
+      substringsArray.push(
+        input.slice(input.indexOf("{"), input.indexOf("}") + 1)
+      );
+      leftCurly = 0;
+      rightCurly = 0;
+    }
+  }
+  if (
+    leftCurly +
+      rightCurly +
+      leftParens +
+      rightParens +
+      leftSquare +
+      rightSquare !==
+    0
+  )
+    return false;
 
-console.log(balancedParens('[](){}')); // true
-console.log(balancedParens('[({})]'));   // true
-console.log(balancedParens('[(]{)}')); // false
+  //console.log("substringsArray", substringsArray);
+  //for each element in substringsArray, iterate thru substring starting from 2nd char until 2nd before last character
+  let output = substringsArray.map(sub => {
+    let counts = {
+      lParens: 0,
+      rParens: 0,
+      lSquare: 0,
+      rSquare: 0,
+      lCurly: 0,
+      rCurly: 0
+    };
+    for (let j = 0; j < sub.length; j++) {
+      if (sub[j] == "(") counts.lParens++;
+      if (sub[j] == ")") counts.rParens++;
+      if (sub[j] == "[") counts.lSquare++;
+      if (sub[j] == "]") counts.rSquare++;
+      if (sub[j] == "{") counts.lCurly++;
+      if (sub[j] == "}") counts.rCurly++;
+    }
+    //console.log("counts", counts);
+    if (counts.lParens !== counts.rParens) return false;
+    else if (counts.lSquare !== counts.rSquare) return false;
+    else if (counts.lCurly !== counts.rCurly) return false;
+    return true;
+  });
+  //console.log("output", output);
+  if (output.includes(false)) return false;
+  else return true;
+}
 
 module.exports = balancedParens;

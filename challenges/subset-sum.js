cb867b2e87da6c6b5f6e90d593278118c5a55b86
tang <eytang8@gmail.com>
2019-02-22 11:18:05 -0800

    finished subset-sum

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -8,8 +8,20 @@
  * subsetSum([8, -2, 1, -3], 6) -> true, 8 + 1 + (-3) = 6
  */
 
-function subsetSum(array, target) {
 
+function subsetSum(array, target, sum = 0 ) {
+  if (target === sum) return true;
+  if (!array) return false;
+  let subArray = [];
+  for (let i = 0; i < array.length; i++){
+    subArray = array.slice(0, i).concat(array.slice(i+1));
+    if(subsetSum(subArray, target, sum + array[i])) return true;
+  }
+  return false;
 }
 
+console.log( subsetSum([3, 7, 4, 2], 5) + ' expect: true, 3 + 2 = 5')
+console.log(subsetSum([3, 34, 4, 12, 5, 12], 32) + ' expect: true, 3 + 12 + 5 + 12 = 32')
+console.log(subsetSum([8, 2, 4, 12], 13) + ' expect: false');
+console.log(subsetSum([8, -2, 1, -3], 6) + '  true, 8 + 1 + (-3) = 6')
 module.exports = subsetSum;

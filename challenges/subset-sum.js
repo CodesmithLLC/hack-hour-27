f705fcec1317622ade043da5b9083fe78dc7d1a4
Chris Air <sdchrisn@gmail.com>
2019-02-22 11:05:06 -0800

    Passed self tests, ready for eval

--- challenges/subset-sum.js
+++ challenges/subset-sum.js
@@ -9,7 +9,37 @@
  */
 
 function subsetSum(array, target) {
+    const data = array.sort().reverse();
+    let sum = 0;
 
+    for(let i = 0; i < data.length; i++){
+        // console.log('loop');
+        if(data[i] === target) return true;
+
+        // if(data[i] > target) continue;
+
+        // if(data[i] < target){
+        else{
+            sum += data[i];
+            // console.log(sum);
+            if(sum === target) return true;
+
+            if(subsetSum(data.slice(i+1), target - sum) === true){
+                return true;
+            }else{
+                sum -= data[i];
+            }
+        }
+    }
+
+    return false;
 }
 
+
 module.exports = subsetSum;
+
+// console.log(subsetSum([3, 7, 4, 2], 5)); // - > true, 3 + 2 = 5
+
+// console.log(subsetSum([3, 34, 4, 12, 5, 12], 32)); // -> true, 3 + 12 + 5 + 12 = 32
+// console.log(subsetSum([8, 2, 4, 12], 13)); // -> false
+// console.log(subsetSum([8, -2, 1, -3], 6)); // -> true, 8 + 1 + (-3) = 6

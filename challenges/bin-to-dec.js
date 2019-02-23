4101aba789ff1e383e18f1d1c5a9d892ef192ab9
Parker Allen <parkerjallen1@gmail.com>
2019-02-23 09:45:52 -0800

    bintodec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,17 @@
  */
 
 function binToDec(binary) {
-
+    let slots = binary.length;
+    let base2 = new Array(slots).fill(0);
+    let decimal = 0;
+    for (let i = 0; i < base2.length; i ++) {
+        base2[i] = 2 ** (slots-1);
+        slots -= 1;
+        decimal += binary[i]*base2[i]
+    }
+    return decimal;
 }
 
+
+console.log(binToDec('101'))
 module.exports = binToDec;

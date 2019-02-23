6cac1ae634acf56c89ace07e0b658d3101da58e9
Sterling Deng <sterlingdeng@gmail.com>
2019-02-23 09:28:55 -0800

    binary to decimal challenge

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -3,7 +3,7 @@
  * Write a function that converts the binary string to a decimal number
  *
  * Example:
-* 	binToDec('0')   -> 0
+ * 	binToDec('0')   -> 0
  * 	binToDec('11')  -> 3
  * 	binToDec('100') -> 4
  * 	binToDec('101') -> 5
@@ -14,7 +14,14 @@
  */
 
 function binToDec(binary) {
-
+  const binArr = Array.from(binary);
+  let counter = 0;
+  let ans = 0;
+  while (binArr.length) {
+    ans += 2 ** counter * binArr.pop();
+    counter += 1;
+  }
+  return ans;
 }
 
 module.exports = binToDec;

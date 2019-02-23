086158f90d12dbe0bdd9e759bc85bb6d417de7e1
Chris Air <sdchrisn@gmail.com>
2019-02-23 10:00:27 -0800

    Completed basic challenge, ready for eval

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,27 @@
  */
 
 function binToDec(binary) {
+    const input = binary.split('');
+    let sum = 0;
+    let exp = 0;
+    while(input.length){
+        if(input.pop() === '1')
+            sum += Math.pow(2, exp);
+        exp++;
+    }
+    return sum;
+    // return parseInt('0101', 2);
+}
 
+function decToBin(decimal){
+    // return decimal.toString(2);
 }
 
 module.exports = binToDec;
+
+// console.log(binToDec('0'));   //-> 0)
+// console.log(binToDec('11'));  //-> 3
+// console.log(binToDec('100')); //-> 4
+// console.log(binToDec('101')); // -> 5
+// console.log(binToDec('0101')); // -> 5
+// console.log(parseInt('0101', 2)); // -> 5

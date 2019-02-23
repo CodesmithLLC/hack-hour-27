3d51ab88f54ec3d968c21fd8732f3495fe474367
Shane Taylor <shaneallantaylor@gmail.com>
2019-02-23 10:00:19 -0800

    bin to dec

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,6 +14,38 @@
  */
 
 function binToDec(binary) {
+  if (binary === "0") {
+    return 0;
+  } else if (binary === "1") {
+    return 1;
+  } else {
+    let output = 0;
+    // determine the length of the input string
+    // length of the input string provides value assigned with each position
+
+    let powerScale = binary.length - 1;
+    // generate an array that has the values of each position in binary
+    function calculateBinArray(n) {
+      let result = 1;
+      while (n--) {
+        result *= 2;
+        binArray.push(result);
+      }
+      return result;
+    }
+    const binArray = [1];
+    calculateBinArray(powerScale);
+
+    // with the binArray, we can compare the string we recieved to the bin array and caluculate the output
+    const inputArray = [...binary].reverse();
+    for (let i = 0; i < inputArray.length; i += 1) {
+      if (inputArray[i] !== '0') {
+        output += binArray[i];
+      }
+    }
+
+    return output;
+  }
 
 }
 

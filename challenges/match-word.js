41082b1c9badd632b181f816e3880aa98c3c1a2f
Chris Air <sdchrisn@gmail.com>
2019-02-26 11:54:16 -0800

    Partially completed challenge and failing on matching the open and close properly

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,7 +11,41 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+    if(str=== '') return true;
 
+    var cleanString = str.replace(/[|&;$%@"<>\]\[()+,-]/g, "_").toLowerCase();
+
+    // console.log(cleanString);
+    // console.log(cleanString.split('_'));
+
+    // console.log(cleanString.split('_')[2]);
+    // console.log(cleanString.split('_')[2].split('').reverse().join(''));
+
+
+    let words = cleanString.split('_');
+    let openWord = words.shift();
+    while(words.length){
+        if(openWord === ''){
+            openWord = words.shift();
+            continue;
+        } 
+    // for(let i = 0; i < words.length; i++){
+        for(let i = 0; i < words.length; i++){
+            let closeWord = words[i].split('').reverse().join('');
+            if(openWord === closeWord)
+                return true;
+        }
+        openWord = words.shift();
+    }
+    return false;
 }
 
 module.exports = matchWord;
+
+// console.log(matchWord('__END_DNE-----'));
+// console.log(matchWord('__ENDDNE____'));
+// console.log( matchWord('IF()()fi[]'));  //-> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi')); // -> false     not properly closed. like ( [) ] 
+
+// console.log( matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  //-> true
+// console.log( matchWord(''));  //-> true

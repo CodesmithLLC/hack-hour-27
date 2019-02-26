99f6c9be93c5ee579ef20d479eec0b829e5cb684
yudataguy <yudataguy@gmail.com>
2019-02-26 10:04:42 -0800

    match-word

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -11,6 +11,27 @@
 // matchWord('');  -> true
 
 function matchWord(str) {
+  //seperate the word and place it in array
+  // loop through array stack word on top
+  // then check for the reverse, if reverse is found, pop it off 
+  // if stack is not empty, return false 
+  let strArr = str.split(/[^a-zA-Z]/g)
+  strArr = strArr.filter(Boolean)
+
+  if (strArr.length <= 1) return false
+  let wordStack = [strArr[0]]
+  for (let i = 1; i < strArr.length; i++){
+
+    if (wordStack[i-1].split('').reverse().join('') === strArr[i]){
+      wordStack.pop()
+      
+    } else {
+      wordStack.push(strArr[i])
+    }
+  }
+  
+  if (wordStack.length === 0) return true
+  else return false
 
 }
 

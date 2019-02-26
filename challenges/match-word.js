58cf005855fa4dfa79f11d7500914dd71cb6120a
Ray Yao <rocaray51@gmail.com>
2019-02-26 13:00:47 -0800

    Update match-word.js

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -12,12 +12,17 @@
 
 function matchWord(str) {
   //clean string by replacing all punctuation with a space.
-  let cleanStr = str.replace(/[^a-z \ \!\?\.\_]/gi, ''); //remove all non-punctuation special chars
-  cleanStr = cleanStr.replace(/[\!\?\.\_]/g, ' ');
-  cleanStr = cleanStr.replace(/\ {2,}/g, ' ');
-  //console.log('cleanStr', cleanStr)
+  let cleanStr = str.replace(/[^a-z\ \!\?\.\_\(\)]/gi, ''); //remove all non-punctuation special chars
+  cleanStr = cleanStr.replace(/[\!\?\.\_\(\)]/g, ' '); //replace all punctuation with a space
+  cleanStr = cleanStr.replace(/\ {2,}/g, ' ').trim().toLowerCase(); //remove leading and trailing spaces, and lowercase everything
+  // console.log('cleanStr', cleanStr)
   //strat: iterate thru string
-  if (cleanStr == '') return true;
+      //if you find a character, keep going until you find a non-character. This is our word.
+    // reverse the word, store it for reference.
+  //recursively slice off word from start and reverse word from end.
+  //if you have empty string at end, return true
+    //if string not empty at end, return false
+  if (cleanStr === '') return true;
   let words = [];
   let word = '';
   for (let i = 0; i < cleanStr.length; i++){
@@ -29,27 +34,41 @@ function matchWord(str) {
     }
     if (i == cleanStr.length - 1) words.push(word);
   }
-  //console.log('words', words)
-  return helperFunc(words);
-    //if you find a character, keep going until you find a non-character. This is our word.
-    // reverse the word, store it for reference.
-  //recursively slice off word from start and reverse word from end.
-  //if you have empty string at end, return true
-    //if string not empty at end, return false
+  // console.log('words', words)
+  let stack = [words[0]];
+  for (let i = 1; i < words.length; i++){
+    
+    stack.push(words[i]);
+    // console.log('stack inside loop', stack)
+    if (stack[stack.length - 1] === stack[stack.length - 2].split('').reverse().join('')) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  //console.log('stack', stack)
+
+    if (stack.length) return false;
+    else return true;
 }
 
-function helperFunc(array){
- if (!array.length) return true;
-  let element = array[0];
-  let reverseElement = element.split('').reverse().join('');
-  //console.log('reverseElement', reverseElement)
-  if (!array.indexOf(reverseElement)) return false;
-  array.splice(array.indexOf(element), 1); //remove word from array
-  array.splice(array.indexOf(reverseElement), 1); //remove word's pair from array
-  let newArray = [...array]
-  //console.log(newArray);
-  return helperFunc(newArray)
-}
+// function helperFunc(array){
+//  if (!array.length) return true;
+//   let element = array[0];
+//   let reverseElement = element.split('').reverse().join('');
+//   //console.log('reverseElement', reverseElement)
+//   if (!array.indexOf(reverseElement)) return false;
+//   array.splice(array.indexOf(element), 1); //remove word from array
+//   array.splice(array.indexOf(reverseElement), 1); //remove word's pair from array
+//   let newArray = [...array]
+//   //console.log(newArray);
+//   return helperFunc(newArray)
+// }
 
-// matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  //-> true
+//   console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));  //-> true
+//  console.log(matchWord('__END_DNE-----')); // -> true
+//   console.log(matchWord('__ENDDNE__')); // -> false       (not separated by a space)
+//   console.log(matchWord('IF()()fi[]')); // -> true        (should be case-insensitive)
+// console.log(matchWord('for__if__rof__fi')); // -> false     not properly closed. like ( [) ] 
+//   console.log(matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw'));//  -> true
+//   console.log(matchWord('')); // -> true
 module.exports = matchWord;

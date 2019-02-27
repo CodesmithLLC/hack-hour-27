764d740c769b2b339f5c6a5885d78231ccaf894f
mlu456 <michaellu213@gmail.com>
2019-02-27 09:56:58 -0800

    typo

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,13 +3,14 @@
  */
 
 function highestProduct(array) {
-  if (!Array.isArray(Array)) return '0';
-  if (array.length < 3) return '0';
+  if (!Array.isArray(array)) return 0;
+  if (array.length < 3) return 0;
 
   let sorted = array.sort((a, b) => a - b);
+  // console.log(sorted)
   return Math.max(sorted[sorted.length - 1] * sorted[sorted.length - 2] * sorted[sorted.length - 3], sorted[0] * sorted[1] * sorted[sorted.length - 1]);
 }
 
 module.exports = highestProduct;
 
-// console.log(highestProduct([-10, 10]));
+// console.log(highestProduct([-10, 10, -20, -10]))

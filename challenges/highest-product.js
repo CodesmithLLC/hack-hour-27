af3380ac0c012590efd82e360318903463d37c2f
mlu456 <michaellu213@gmail.com>
2019-02-27 09:52:04 -0800

    return string 0??

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,8 @@
  */
 
 function highestProduct(array) {
-  if (!Array.isArray(Array)) return 0;
-  if (array.length < 3) return 0;
+  if (!Array.isArray(Array)) return '0';
+  if (array.length < 3) return '0';
 
   let sorted = array.sort((a, b) => a - b);
   return Math.max(sorted[sorted.length - 1] * sorted[sorted.length - 2] * sorted[sorted.length - 3], sorted[0] * sorted[1] * sorted[sorted.length - 1]);

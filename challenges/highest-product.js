6246c6d1decf4b14d133a536a61f13c744035a19
tang <eytang8@gmail.com>
2019-02-27 09:34:25 -0800

    added edge case handling

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,11 @@
  */
 
 function highestProduct(array) {
+  // why should we return a 0 instead of throwing an error?
+  if (!Array.isArray(array) return 0;
+  // This also makes no sense, but the test insists I do it.
+  if (array.length < 3) return 0;
+  
   const sorted = array.slice().sort((a,b)=>a-b);
   const topTwoProd = sorted[sorted.length-1] * sorted[sorted.length-2];
   const botTwoProd = sorted[0] * sorted[1];

74d9f3a75435c2c6d1bbfe8e392dfedd2bf02969
Augustine Kim <ajk830@gmail.com>
2019-02-27 09:23:09 -0800

    highest-product first attempt

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,19 @@
  */
 
 function highestProduct(array) {
+  if (array.length < 3) return undefined;
+  array.sort((a, b) => b - a );
 
+  if (array[array.length-1] * array[array.length-2] > array[0] * array[1]) {
+    return array[array.length-1] * array[array.length-2] * array[0];
+  } else {
+    return array[0] * array[1] * array[2];
+  }
 }
 
 
 module.exports = highestProduct;
+
+
+
+// console.log(highestProduct([-5, -4, 1, 2, 3, 4]));

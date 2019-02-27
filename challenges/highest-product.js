6f058285b0a2e56cf09d6efae01f65e05aed1acc
mlu456 <michaellu213@gmail.com>
2019-02-27 09:23:31 -0800

    highest prod

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,10 @@
  */
 
 function highestProduct(array) {
-
+  let sorted = array.sort((a, b) => a - b);
+  return Math.max(sorted[sorted.length - 1] * sorted[sorted.length - 2] * sorted[sorted.length - 3], sorted[0] * sorted[1] * sorted[sorted.length - 1]) 
 }
 
-
 module.exports = highestProduct;
+
+// console.log(highestProduct([-10, -10, 1, 2, 3, 10]))

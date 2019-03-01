06994f8faa39b3ce9a445b50f803becb702f90f3
Chris Air <sdchrisn@gmail.com>
2019-03-01 09:50:51 -0800

    First attempt, curious to see how it does against the test runner

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,39 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let ptrL1 = l1;
+  let ptrL2 = l2;
+  let alternate = true;
+
+  if(l1 === null && l2 === null) return new Node();
+  if(l1 === null) return l2;
+  if(l2 === null) return l1;
+
+  const result = new Node(ptrL1.value);
+  ptrL1 = ptrL1.next;
+  let ptrResult = result;
+  while(ptrL1 !== null || ptrL2 !== null) {
+    if(ptrL1 === null){ // l1 NULL, NOT L2
+      ptrResult.next = new Node(ptrL2.value);
+      ptrL2 = ptrL2.next;
+    } else if (ptrL2 === null) { // l1 NULL, NOT L2
+      ptrResult.next = new Node(ptrL1.value);
+      ptrL1 = ptrL1.next;
+    }
+    else {  // BOTH NOT NULL
+      if(alternate){ // handle the L2
+        ptrResult.next = new Node(ptrL2.value);
+        ptrL2 = ptrL2.next;
+      }else{
+        // handle the L1
+        ptrResult.next = new Node(ptrL1.value);
+        ptrL1 = ptrL1.next;
+      }
+      alternate = !alternate;
+    }
+    ptrResult = ptrResult.next;
+  }
+  return result;
 };
 
 module.exports = {Node: Node, zip: zip};

764b06bb5cb40ef8a07d884091f4dcb7e717afe2
Shane Taylor <shaneallantaylor@gmail.com>
2019-03-02 09:52:06 -0800

    max stack baaaaaabeeeee

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,42 @@
  */
 
 function Stack() {
-  // body...
+  this.curLength = 0;
+  this.storage = [];
+  this.curMax = -Infinity;
+  this.mostRecent;
+
+  this.push = function (...val) {
+    if (val.length === 1) {
+      this.storage = [...this.storage, val];
+      this.curLength += 1;
+      this.curMax = Math.max(...this.storage);
+      return this.curLength;
+    } else {
+      for (let i = 0; i < val.length; i += 1) {
+        this.storage = [...this.storage, val[i]];
+        this.curLength += 1;
+      }
+      this.curMax = Math.max(...this.storage);
+      return this.curLength;
+    }
+  };
+
+  this.pop = function () {
+    if (this.curLength === 0) {
+      return "There is nothing in this stack";
+    } else {
+      this.curLength -= 1;
+      const output = this.storage[this.curLength];
+      this.storage.length = this.curLength;
+      this.curMax = Math.max(...this.storage);
+      return output;
+    }
+  }
+
+  this.getMax = function () {
+    return this.curMax;
+  }
 }
 
 module.exports = Stack;

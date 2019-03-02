469cc7c6ade1d04ccab8ab092b3eace02881acc9
Ray Yao <rocaray51@gmail.com>
2019-03-02 15:58:56 -0800

    zip-linked-list from solution

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -10,64 +10,26 @@ function Node(val) {
   this.next = null;
 }
 
-function Node(val) {
-  this.value = val;
-  this.next = null;
-}
-
 function zip(l1, l2) {
   if (!l1 && !l2) return undefined;
   if (!l1) return l2;
   if (!l2) return l1;
-//Approach 1: pointer alternate
-//start at l1
-//store l1 next in temp
-// set l1 next to l2
-// set l2 next to temp
-// reassign "l1" to temp
-/* let pointer = l1;
-while (pointer) {
-  let temp = pointer.next;
-  l1.next = l2;
-  l2.next = temp;
-  pointer = temp
+  let head = l1;
+  let temp = l1;
+  l1 = l1.next;
+  while (l1 && l2){
+    temp.next = l2;
+    temp = temp.next;
+
+    l2 = l2.next;
+    temp.next = l1
+
+
+    temp = temp.next;
+    l1 = l1.next;
+  }
+  temp.next = !l1 ? l2 : l1;
+  return head;
 }
-return l1; */
-
-//Approach 2: storage arrays
-//store all values into separate arrays
-//combine arrays, alternating
-//create new LL using combined
-let vals1 = [];
-let vals2 = [];
-let pointer = l1;
-while (pointer) {
-  vals1.push(pointer.value)
-  pointer = pointer.next
-}
-pointer = l2;
-while (pointer) {
-  vals2.push(pointer.value)
-  pointer = pointer.next;
-} 
-console.log('vals1', vals1)
-console.log('vals2', vals2)
-
-//Approach 3: build new LL
-// let output = new Node(l1.value);
-// let pointer = l1;
-// while (pointer){
-// 	output.next 
-// }
-
-};
-
-
-let l1 = new Node(1);
-l1.next = new Node(3);
-l1.next = new Node(5);
-let l2 = new Node(2);
-l2.next = new Node(4);
-console.log(l1)
 
 module.exports = {Node: Node, zip: zip};

ae28be80142dcc980e1d33518a757e9598a15e73
Parker Allen <parkerjallen1@gmail.com>
2019-03-05 09:46:52 -0800

    reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,18 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    let nodes = [];
+    while (head) {
+        nodes.push(head.value)
+        head = head.next;
+    }
+    let reverse = new Node(nodes[nodes.length-1]);
+    const reverseHead = reverse;
+   for (let i = nodes.length - 2; i >= 0; i--) {
+       reverse.next = new Node(nodes[i]);
+       reverse = reverse.next;
+   }
+   return reverseHead;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

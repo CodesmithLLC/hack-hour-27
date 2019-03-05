5adb8f1d78947397884d6498f2d18b770c6661ae
Ed <15ryane@gmail.com>
2019-03-05 09:50:35 -0800

    O(n) solution

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,53 +13,80 @@ function Node(value) {
     this.next = null;
 }
 
-function reverseLinkedList(head) {
+function reverseLinkedList(node, nextNode, firstIteration = true) {
 
-  const list = [];
+	console.log(node)
 
-  const traverseAndSave = function(node) {
-    if(node.next === null) return list.unshift(node);
-    else {
-      list.unshift(node);
-      traverseAndSave(node.next);
-    }
+  // edge case - 1 member list
+  if(node.next === null) return node;
+
+  // is this the first iteration?
+  // yes - meet your new tail!
+  if(firstIteration === true){
+  	let tempNode = node.next;
+  	let nextTempNode = node.next.next;
+  	tempNode.next = node;
+  	node.next = null;
+  	return reverseLinkedList(tempNode, nextTempNode, false)
   }
-
-  traverseAndSave(head);
-
-  for(let i=0; i<list.length; i++){
-  	list[i].next = list[i+1];
-  }
-
-  list[list.length-1].next = null;
-
-  return list[0];
+  // recu case
+  // does node.next exist?
+  // yes - move the pointers ahead
+  // then make the nextNode point backwards
+  // recu call on the next pointers
+  if(nextNode){
+  	let tempNode = nextNode;
+  	let nextTempNode = nextNode.next;
+  	nextNode.next = node;
+  	return reverseLinkedList(tempNode, nextTempNode, false)
+  // base case
+  // no - return the current node - you're at the new head!
+  } else {
+  	return node;
+  }  
 
 }
 
 
+
 //testing
 
-// let aa = new Node(1);
-// let ab = new Node(2);
-// let ac = new Node(3);
-// let ad = new Node(4);
-// let ae = new Node(5);
-// let af = new Node(6);
-// let ag = new Node(7);
+// long list
+	// let aa = new Node(1);
+	// let ab = new Node(2);
+	// let ac = new Node(3);
+	// let ad = new Node(4);
+	// let ae = new Node(5);
+	// let af = new Node(6);
+	// let ag = new Node(7);
 
-// aa.next = ab
-// ab.next = ac
-// ac.next = ad
-// ad.next = ae
-// ae.next = af
-// af.next = ag
+	// aa.next = ab
+	// ab.next = ac
+	// ac.next = ad
+	// ad.next = ae
+	// ae.next = af
+	// af.next = ag
 
-// let ba = new Node(1);
-// let bb = new Node(2);
+// 2 member list
+	// let ba = new Node(1);
+	// let bb = new Node(2);
 
-// ba.next = bb
+	// ba.next = bb
 
-// reverseLinkedList(aa);
+// 1 member list
+	// let ca = new Node(1);
+
+
+// const newList = reverseLinkedList(aa);
+
+// function traverse(node) {
+// 	if(node.next === null) console.log(node.value);
+// 	else {
+// 		console.log(node.value);
+// 		traverse(node.next);
+// 	}
+// }
+
+// traverse(newList)
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

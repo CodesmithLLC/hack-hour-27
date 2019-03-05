f53257d33e6a7c8b9d7abe466dd1ae33dbd4b36a
yudataguy <yudataguy@gmail.com>
2019-03-05 09:49:01 -0800

    reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -11,9 +11,28 @@
 function Node(value) {
     this.value = value;
     this.next = null;
+    this.prev = null;
 }
 
 function reverseLinkedList(head) {
+    // move until end of LL
+    // when this.next = null, assign the value to this.value 
+    // remove the lat link, turn previous this.next = null 
+    // base case: node is null
+
+    do {
+        head.next.prev = head
+        head = head.next
+    } while (head.next !== null)
+    
+    const newHead = head
+
+    do {
+        [head.next, head.prev] = [head.prev, head.next]
+        head = head.prev
+    } while (head.prev !== null)
+
+    return newHead
 
 }
 

72a40446bf2b08fbd1e3c9562f1dbd5b02a35db5
Ray Yao <rocaray51@gmail.com>
2019-03-06 00:18:36 -0800

    reverse-linked-list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -15,31 +15,25 @@ function Node(value) {
 
 function reverseLinkedList(head) {
 	if (!head.next) return head;
-  //strat: p1, p2, T start at head. Until tail.next is null: T moves to next, P2 moves to tail, P2.next becomes P1. P1 moves to next. repeat.
-  let tail = head;
-  let prev = head;
-  let prev2 = head;
-  let length = 0;
-  while (tail.next){
-  	tail = tail.next
-    length++;
-  }
-  tail = head.next;
-  for (let i = 0; i <= length; i++){
-    prev2 = prev2.next;
-    tail = tail.next;
-    prev2.next = prev
-  } 
-  console.log('tail', tail)
-  console.log('prev', prev)
-  console.log('prev2', prev2)
-  console.log('length', length)
+  //strat: prev, curr, next; movement: c <- to p, P to C, C to N, N to N.N
+  let prev = null;
+  let current = head;
+  let next = head.next;
+  while (next.next) {
+    current.next = prev;
+    prev = current;
+    current = next;
+    next = next.next;
+  } ;
+  current.next = prev;
+  next.next = current;
+  return next;
 }
 
-let myLL = new Node(1);
-myLL.next = new Node(2);
-myLL.next.next = new Node(3);
-myLL.next.next.next = new Node(4);
-console.log(reverseLinkedList(myLL));
+// let myLL = new Node(1);
+// myLL.next = new Node(2);
+// myLL.next.next = new Node(3);
+// myLL.next.next.next = new Node(4);
+// console.log(reverseLinkedList(myLL));
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};

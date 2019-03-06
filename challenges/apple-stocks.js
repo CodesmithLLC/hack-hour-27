c2e67a98a3459d0194d40330017c84835edde308
Ed <15ryane@gmail.com>
2019-03-06 09:59:59 -0800

    apple stocks v1

--- challenges/apple-stocks.js
+++ challenges/apple-stocks.js
@@ -12,8 +12,55 @@
  *  Return 0 if no profit is possible OR if input is invalid.
  */
 
-function bestProfit(stock_prices_yesterday) {
+function bestProfit(stock_prices_yesterday, pastNumbers = new Set(), largestProfit = 0) {
+
+	// console.log(stock_prices_yesterday)
+	// console.log(pastNumbers)
+
+	let newArray;
+
+	// edge case
+	// we were passed an empty array
+	if (stock_prices_yesterday.length === 0) return largestProfit;
+
+	// base case
+	// the array has only 1 member
+	if (stock_prices_yesterday.length === 1) return largestProfit;
+
+	// recu case
+	// we have 2 or more members in the array
+
+	pastNumbers.add(stock_prices_yesterday[0]);
+	newArray = stock_prices_yesterday.slice(1);
+	
+	// subtract the first member of the newArray against the entire set
+	// if any are larger than the largestProfit, make it the new largestProfit
+
+	// console.log('target: ',newArray[0])
+
+	pastNumbers.forEach( (ele) => {
+		let difference = newArray[0] - ele;
+
+		// console.log(difference)
+
+		if (difference > largestProfit) largestProfit = difference;
+	});
+
+	return bestProfit(newArray, pastNumbers, largestProfit);
 
 }
+
+// tests
+
+// const arrTest = [100, 0];
+// const arrTest = [0, 100];
+// const arrTest = [0];
+// const arrTest = [];
+// const arrTest = [20, 100, 10];
+// const arrTest = [30, 100, 30];
+// const arrTest = [10, 10, 0, 20, 10, 30, 10, 100, 0];
+// console.log(bestProfit(arrTest))
+
+
 
 module.exports = bestProfit;

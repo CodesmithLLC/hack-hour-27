bbf17f11a5680685bdefdd5259c58bf1ea0e6482
Ed <15ryane@gmail.com>
2019-03-07 09:27:43 -0800

    merge-arrays v1

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -13,8 +13,42 @@
  *
  */
 
-function mergeArrays(arr1, arr2) {
+function mergeArrays(arr1, arr2, fullArray = []) {
+
+	// edge cases - missing arguments
+	if(!arr1 && !arr2) return undefined;
+	if(!arr1) return arr2;
+	if(!arr2) return arr1;
+
+	// base case - no more numbers left in an array to sort
+	if(arr1.length === 0) return fullArray.concat(arr2);
+	if(arr2.length === 0) return fullArray.concat(arr1);
+
+	// recu case
+	// compare front numbers from arrs
+	// push the larger one into fullArray
+	// remove it, then call the func recursively with the modified arrays
+	if(arr1[0] <= arr2[0]) {
+		fullArray.push(arr1[0]);
+		newArr1 = arr1.slice(1);
+		newArr2 = arr2.slice();
+		return mergeArrays(newArr1, newArr2, fullArray);
+	} else {
+		fullArray.push(arr2[0]);
+		newArr1 = arr1.slice();
+		newArr2 = arr2.slice(1);
+		return mergeArrays(newArr1, newArr2, fullArray);
+	}
 
 }
+
+// tests
+
+// var my_array = [3,4,6,10,11,15,21];
+// var another_array = [];
+
+// console.log(mergeArrays(my_array, another_array));
+
+
 
 module.exports = mergeArrays;

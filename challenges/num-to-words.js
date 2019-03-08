4f1551719453ac4b8604cf5be5b8a9718a79e520
Shane Taylor <shaneallantaylor@gmail.com>
2019-03-08 09:38:40 -0800

    0 thru 19

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,33 @@
  */
 
 function numToWords(num) {
+  // probably gonna be recursive
+  // convert num to string
+  // eval it's length
 
+  switch (num) {
+    case 0: return 'Zero'
+    case 1: return 'One'
+    case 2: return 'Two'
+    case 3: return 'Three'
+    case 4: return 'Four'
+    case 5: return 'Five'
+    case 6: return 'Six'
+    case 7: return 'Seven'
+    case 8: return 'Eight'
+    case 9: return 'Nine'
+    case 10: return 'Ten'
+    case 11: return 'Eleven'
+    case 12: return 'Twelve'
+    case 13: return 'Thirteen'
+    case 14: return 'Fourteen'
+    case 15: return 'Fifteen'
+    case 16: return 'Sixteen'
+    case 17: return 'Seventeen'
+    case 18: return 'Eighteen'
+    case 19: return 'Nineteen'
+  }
 }
+console.log('numToWords(18) -->', numToWords(18));
 
 module.exports = numToWords;

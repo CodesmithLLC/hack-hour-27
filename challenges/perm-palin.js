bf66eca36923576a0cd12a3fbf6b8d95609d2200
Augustine Kim <ajk830@gmail.com>
2019-03-09 09:38:30 -0800

    perm-palin complete

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,17 @@
  */
 
 function permPalin(str) {
-	
+  let set = new Set();
+  for (let i = 0; i < str.length; i ++) {
+    if (set.has(str.charAt(i))) set.delete(str.charAt(i));
+    else (set.add(str.charAt(i)));
+  }
+  return set.size <= 1;;
 }
 
 module.exports = permPalin;
+
+// console.log(permPalin('abab')); // => true
+// console.log(permPalin('cbaba')); // => true
+// console.log(permPalin('cbac')); // => false
+// console.log(permPalin('a')); // => true

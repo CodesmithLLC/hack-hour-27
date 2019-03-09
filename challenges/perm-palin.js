7cad2f4d6de423144cd6bc418dee5f17e1eb7f65
tang <eytang8@gmail.com>
2019-03-09 09:22:54 -0800

    finished perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
+  const set = new Set();
+  for (let i = 0; i < str.length; i++){
+    if (set.has(str[i])){
+      set.delete(str[i]);
+    }else{
+      set.add(str[i]);
+    }
+  }
+  return !(set.size>1);
 }
+/*
+ console.log(permPalin('abab'), true)
+ console.log(permPalin('cbaba'), true)
+ console.log(permPalin('cbac'), false)
+ console.log(permPalin('a'), true)
+ */
 
 module.exports = permPalin;

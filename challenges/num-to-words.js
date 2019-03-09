6c0b89e9a9a71b4eca2065f950c983fd958ab0ac
justnoah <noahxman@gmail.com>
2019-03-09 09:44:10 -0800

    Completed; perm-palin.js

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -14,6 +14,55 @@
 
 function numToWords(num) {
 
+  let last = num % 1000;
+	console.log('TEST: numToWords -> last', last);
+  let rest = Math.ceil(num / 1000);
+	console.log('TEST: numToWords -> rest', rest);
+
 }
 
+function splitByPlace(num, result = []) {
+  let last = num % 1000;
+  let rest = Math.ceil(num / 1000);
+  if (rest === 0) return result.reverse();
+  result.push(last);
+  // return splitByPlace(rest, result);
+}
+
+
+// console.log(numToWords(5));
+// console.log(numToWords(12));
+// console.log(numToWords(325));
+console.log(numToWords(65265563));
+console.log(splitByPlace(65265563));
+// console.log(numToWords(2362562));
+// console.log(numToWords(252435561));
+// console.log(numToWords(12503566));
+// console.log(numToWords(12503567));
+// console.log(numToWords(12503568));
+// console.log(numToWords(12503569));
+
 module.exports = numToWords;
+
+
+// switch(num) {
+//   case '1': 'One' 
+//     break;
+//   case '2': 'Two' 
+//     break;
+//   case '3': 'Three' 
+//     break;
+//   case '4': 'Four' 
+//     break;
+//   case '5': 'Five' 
+//     break;
+//   case '6': 'Six' 
+//     break;
+//   case '7': 'Seven' 
+//     break;
+//   case '8': 'Eight' 
+//     break;
+//   case '9': 'Nine' 
+//     break;
+
+// if (num === 0) { return 'Zero.'}
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,18 @@
  */
 
 function permPalin(str) {
-	
+  const chars = str.split('');
+  const count = {};
+
+  chars.forEach(char => count[char] = (count[char] || 0) + 1);
+
+  const result = Object.values(count);
+
+  return !!result.reduce((acc, cur) => {
+    if (cur % 2 !== 0) acc -= 1;
+    return acc;
+  }, 2);
 }
 
 module.exports = permPalin;
+

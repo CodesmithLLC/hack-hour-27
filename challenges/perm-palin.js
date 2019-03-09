21194211c73fc635fe3050b729396ebe8d49fa80
dasbus <90valentin@gmail.com>
2019-03-09 09:37:54 -0800

    perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,30 @@
  */
 
 function permPalin(str) {
-	
+
+  let exception = 0
+  
+  let ledger = {}
+  let arr = str.replace(" ", "").split('')
+	arr.forEach((x)=> {
+    if (ledger[x]) ledger[x]++
+    else ledger[x] = 1
+  })
+  for (let v of Object.values(ledger)){
+   if (v % 2 === 0) continue
+   else exception++
+  }
+  console.log('exceptions = ', exception)
+  if (exception > 1) return false;
+  return true;
 }
+
+
+console.log(permPalin('aaabbb'))  //false
+console.log(permPalin('aaaabbbbt')) //false
+console.log(permPalin('aaeabbebt')) //true
+console.log(permPalin('33gghho'))
+console.log(permPalin('nurses run'))
+
 
 module.exports = permPalin;

3d824ca5d98e8ebf2938b9864d40fc1093138a10
Sterling Deng <sterlingdeng@gmail.com>
2019-03-12 09:46:43 -0700

    stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -1,20 +1,49 @@
 /**
  * Create a stack.Then create a queue using two stacks.
  */
-
-
-function Stack() {
-
-}
-
-
+var Stack = /** @class */ (function() {
+  function Stack() {
+    this.s = [];
+  }
+  Stack.prototype.push = function(v) {
+    this.s.push(v);
+  };
+  Stack.prototype.pop = function() {
+    return this.s.pop();
+  };
+  Stack.prototype.isEmpty = function() {
+    return !this.s.length;
+  };
+  return Stack;
+})();
 /**
-* Queue Class
-*/
-
-
-function Queue() {
-
-}
-
-module.exports = {Stack: Stack, Queue: Queue};
+ * Queue Class
+ */
+var Queue = /** @class */ (function() {
+  function Queue() {
+    this.s1 = new Stack();
+    this.s2 = new Stack();
+  }
+  Queue.prototype.enqueue = function(v) {
+    this.s1.push(v);
+  };
+  Queue.prototype.dequeue = function() {
+    while (!this.s1.isEmpty()) {
+      this.s2.push(this.s1.pop());
+    }
+    var dequeued = this.s2.pop();
+    while (!this.s2.isEmpty()) {
+      this.s1.push(this.s2.pop());
+    }
+    return dequeued;
+  };
+  return Queue;
+})();
+// const q = new Queue();
+// // for (let i = 0; i < 10; i += 1) {
+// //   q.enqueue(i);
+// // }
+// // for (let i = 0; i < 10; i += 1) {
+// //   console.log(q.dequeue());
+// // }
+module.exports = { Stack: Stack, Queue: Queue };

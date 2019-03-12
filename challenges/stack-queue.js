0f9279936b42d0af0d2c178033fd2f383a8cd494
yudataguy <yudataguy@gmail.com>
2019-03-12 09:57:17 -0700

    stack que

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,15 @@
 
 
 function Stack() {
-
+  let result = []
+  let length = 0
+  const push = (num) => {
+    return result[length++] = num
+  }
+  const pop =  () => {
+    return result[length--] = null
+  }
+  return result
 }
 
 
@@ -14,7 +22,24 @@ function Stack() {
 
 
 function Queue() {
-
+  let result = new Stack
+  let reverseResult = new Stack
+  let length = 0
+  const dequeue = () => {
+    if (this.length > 0) {
+      reverseResult = result.reverse().pop().reverse()
+      result = reverseResult.reverse()
+      this.length--
+      return result
+    }
+    return result
+  }
+  const enqueue = (num) => {
+    result.push(num)
+    reverseResult = result.reverse()
+    this.length++
+    return result
+  }
 }
 
 module.exports = {Stack: Stack, Queue: Queue};

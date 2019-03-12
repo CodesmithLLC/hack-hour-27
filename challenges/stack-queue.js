d269f0e5aab2f8ac895ebac72293951f54423230
Ray Yao <rocaray51@gmail.com>
2019-03-12 09:25:53 -0700

    submit to see test cases

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,24 @@
 
 
 function Stack() {
+  this.storage = [];
+  this.length = 0;
+}
 
+Stack.prototype.push = function(elem){
+  this.storage.push(elem); 
+}
+
+Stack.prototype.pop = function(elem){
+  return this.storage.pop();
+}
+
+Queue.prototype.enqueue = function(elem){
+  this.storage.push();
+}
+
+Queue.prototype.dequeue = function(elem){
+  return this.storage.shift();
 }
 
 
@@ -14,7 +31,8 @@ function Stack() {
 
 
 function Queue() {
-
+  this.storage = new Stack();
+  this.length = 0;
 }
 
 module.exports = {Stack: Stack, Queue: Queue};

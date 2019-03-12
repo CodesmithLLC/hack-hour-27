7fa35447c7b93f8bc037c72930c518ff74d40e50
tang <eytang8@gmail.com>
2019-03-12 09:51:20 -0700

    finished stack queue

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,13 @@
 
 
 function Stack() {
-
+  this.store = [];
+  this.push = function(item){
+    return this.store.push(item);
+  }
+  this.pop = function(){
+    return this.store.pop()
+  };
 }
 
 
@@ -14,7 +20,22 @@ function Stack() {
 
 
 function Queue() {
+  this.input = new Stack()
+  this.output = new Stack()
+  this.dequeue = function(){
+    if(this.output.store.length === 0){
+      while (this.input.store.length){
+            const thing = this.input.pop();
+            this.output.push(thing);
+          }
+      }
+    return this.output.pop();
+  }
 
+  this.enqueue = function(item){
+    this.input.push(item);
+  }
 }
+
 
 module.exports = {Stack: Stack, Queue: Queue};

cc6714c8b04046fef4e987e06f74e356c026ca53
Augustine Kim <ajk830@gmail.com>
2019-03-13 09:16:37 -0700

    two-sum complete

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,15 @@
  */
 
 function twoSum(arr, n) {
-
+  const memo = new Set();
+  for (let value of arr) {
+    if (memo.has(value)) return true;
+    else memo.add(n-value);
+  }
+  return false;
 }
 
 module.exports = twoSum;
+
+// console.log(twoSum([2,5,3,9,10], 13));
+// console.log(twoSum([2,5,3,9,7], 13));

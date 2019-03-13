6ab4282b4b5d4a8c0be4cd5420d1ccc8d2c33861
Ed <15ryane@gmail.com>
2019-03-13 09:23:26 -0700

    twosum v1

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,6 +4,19 @@
 
 function twoSum(arr, n) {
 
+  const complement = new Set();
+
+  for(let i=0; i<arr.length; i++){
+    if (complement.has(arr[i])) return true;
+    else complement.add(n - arr[i]);
+  }
+
+  return false;
+
 }
+
+console.log(twoSum([1,2,3,4,5], 3))
+
+
 
 module.exports = twoSum;

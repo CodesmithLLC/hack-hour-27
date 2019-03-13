f8df95068b3c972c4b817828de0ec49d2902d6d0
Shane Taylor <shaneallantaylor@gmail.com>
2019-03-13 09:45:38 -0700

    two sum with set and forEach

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,6 +3,33 @@
  */
 
 function twoSum(arr, n) {
+  // hello old friend
+  // weve got a target number, yay!
+  let output = false;
+  const compliments = new Set();
+
+  // lets iterate through the array
+  arr.forEach(cur => {
+    // if the cur value matches n, WE RETURN TRUE
+    if (cur === n) {
+      output = true;
+    } else {
+      // if the cur value doesnt match n, we subtract cur value from n and get compliment number
+      let comp = n - cur;
+
+      // we check the compliments object for the cur number. if its there WE RETURN TRUE
+      if (compliments.has(cur)) {
+        output = true;
+      } else {
+        // if not, we put the compliment in the compliments object
+        compliments.add(comp);
+      }
+    }
+    // we move on to the next cur value
+  });
+
+  // if, AFTER ALL THAT, we dont return true, WE RETURN FALSE
+  return output;
 
 }
 

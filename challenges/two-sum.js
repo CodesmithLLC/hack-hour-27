649f8057e4232f659ccbbae72179d7102ad829b1
mlu456 <michaellu213@gmail.com>
2019-03-13 09:16:37 -0700

    two-sum first approach

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,14 @@
  */
 
 function twoSum(arr, n) {
-
+  let seen = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (seen.has(n - arr[i])) {
+      return true;
+    }
+    seen.add(arr[i]);
+  }
+  return false;
 }
 
 module.exports = twoSum;

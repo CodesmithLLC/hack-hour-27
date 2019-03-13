3576454e218c10bbf561b45d8bf70efb29f6c920
dasbus <90valentin@gmail.com>
2019-03-13 09:32:59 -0700

    two-sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,25 @@
  */
 
 function twoSum(arr, n) {
+  let result = false
+  let newArr = []
+  newArr = arr.map(number => {
+    return n - number
+  })
 
+  arr.forEach(number => {
+    if (arr.includes(n - number)){
+      result = true;
+    }
+  })
+    
+  return result 
 }
 
 module.exports = twoSum;
+
+
+array1 = [12, 3, 4, 45]
+target = 18;
+
+console.log(twoSum(array1, target));

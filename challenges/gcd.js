09f868845e513b92abac73c374f3d531142613b7
tang <eytang8@gmail.com>
2019-03-15 09:43:27 -0700

    finished gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,27 @@
  */
 
 function gcd(a, b) {
-
+  if (a === b) return a;
+  let low;
+  let high;
+  let lookAt;
+  if (a > b){
+    high = a;
+    low = b;
+  } else {
+    low = b;
+    high = a;
+  }
+  if (high/2 > low){
+    lookAt = low;
+  } else {
+    lookAt = Math.ceil(high/2);
+  }
+  for (let i = lookAt; i > 0; i--){
+    if(!(a%i) && !(b%i)){
+      return i;
+    }
+  }
 }
 
 module.exports = gcd;

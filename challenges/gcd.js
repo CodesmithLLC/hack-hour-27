5e9570574ae2d4f2432448f6919c57cde9fc3579
Parker Allen <parkerjallen1@gmail.com>
2019-03-15 10:00:01 -0700

    greatest common denominator

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,18 @@
  */
 
 function gcd(a, b) {
+  if (a === 0 || b === 0) return (a || b)
+  let denominator;
+  const set = new Set()
 
+  for (let i = a; i >= 1; i--) {
+    if (a % i === 0) set.add(i);
+  }
+
+  for (let i = b; i >= 1; i--) {
+    if (b % i === 0) denominator = i;
+    if (set.has(denominator)) return denominator;
+  }
 }
 
 module.exports = gcd;

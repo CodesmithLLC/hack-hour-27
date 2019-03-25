efeebc73827532d1b6406ce93778ecd87555b41d
Augustine Kim <ajk830@gmail.com>
2019-03-25 09:23:43 -0700

    count-twos complete

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -10,7 +10,18 @@
 
 
 function countTwos(num) {
-
+  let count = 0;
+  for (let i = 2; i <= num; i++) {
+    count += (i.toString().match(/2/g) || []).length;
+  }
+  return count;
 }
 
 module.exports = countTwos;
+
+
+// console.log(countTwos(1)); //  -> 0
+// console.log(countTwos(3)); //  -> 1
+// console.log(countTwos(13)); //  -> 2
+// console.log(countTwos(1000)); //  -> 300
+// console.log(countTwos(11420)); //  -> 4483

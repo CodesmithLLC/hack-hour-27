baf18538cf72218fb9ecfc836130f82766b897b8
Parker Allen <parkerjallen1@gmail.com>
2019-03-26 09:48:24 -0700

    add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,46 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let math = [];
+  let curr1 = l1
+  let curr2 = l2
 
+  while (curr1 && curr2) {
+    math.push((curr1.value + curr2.value))
+    curr1 = curr1.next;
+    curr2 = curr2.next;
+  }
+
+  while(curr1) {
+    math.push(curr1.value)
+    curr1 = curr1.next;
+  }
+
+  while(curr2) {
+    math.push(curr2.value)
+    curr2 = curr2.next;
+  }
+  
+  for (let i = 0; i < math.length; i++) {
+    if (math[i] >= 10) {
+      math[i] = math[i]%10;
+      if (!math[i+1]) {
+        math[i+1] = 1; 
+      } else {
+        math[i+1] += 1;
+      } 
+    }  
+  }
+
+  const output = new Node(math[0])
+  let current = output;
+
+  for (let i = 1; i < math.length; i++) {
+    current.next = new Node(math[i]);
+    current = current.next;
+  }
+
+  return output
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};

0e75fc6feff516ac4b1216d8a051abb51dbf4262
Augustine Kim <ajk830@gmail.com>
2019-03-26 09:28:19 -0700

    added-linked-list complete

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,47 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  let output = new Node();
+  let p1 = l1, p2 = l2, pOut = output;
+  let carry = 0;
 
+  while (p1 || p2 || carry) {
+    let sum = carry;
+    if (p1) {
+      sum += p1.value;
+      p1 = p1.next;
+    }
+
+    if (p2) {
+      sum += p2.value;
+      p2 = p2.next;
+    }
+    
+    if (sum >= 10) {
+      carry = 1;
+      pOut.value = sum % 10;
+    } else {
+      carry = 0;
+      pOut.value = sum;
+    }
+
+    if (p1 || p2 || carry) pOut = pOut.next = new Node();
+  }
+
+  return output;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
+
+// const l1 = new Node(2);
+// l1.next = new Node(1);
+// l1.next.next = new Node(5);
+// const l2 = new Node(5);
+// l2.next = new Node(9);
+// l2.next.next = new Node(4);
+// const added = addLinkedList(l1, l2);
+// let p = added;
+// while (p) {
+//   console.log(p.value);
+//   p = p.next;
+// }

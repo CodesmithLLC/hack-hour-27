d2427a212993e24969b8ccd21733139da9aa390f
mlu456 <michaellu213@gmail.com>
2019-03-27 09:05:29 -0700

    add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,48 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
 
+  let carry = 0;
+  let curl1 = l1;
+  let curl2 = l2;
+  let initVal = curl1.value + curl2.value;
+  if (initVal >= 10) {
+    carry = 1;
+    initVal -= 10;
+  }
+  let res = new Node(initVal);
+  let curNode = res;
+
+  while (curl1.next && curl2.next) {
+    curl1 = curl1.next;
+    curl2 = curl2.next;
+    let value = curl1.value + curl2.value + carry;
+    if (value >= 10) {
+      carry = 1;
+      value -= 10;
+    } else {
+      carry = 0;
+    }
+
+    let node = new Node(value);
+    
+    curNode.next = node;
+    curNode = curNode.next;
+  }
+
+  return res;
 }
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
+
+const l1 = new Node(2);
+l1.next = new Node(1);
+l1.next.next = new Node(5);
+
+const l2 = new Node(5);
+l2.next = new Node(9);
+l2.next.next = new Node(2);
+
+console.log(addLinkedList(l1, l2))

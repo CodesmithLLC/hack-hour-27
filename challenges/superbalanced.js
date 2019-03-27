2921c0d6df8150bf6b5654fbed95e2c3b4f5d4ae
Augustine Kim <ajk830@gmail.com>
2019-03-27 09:21:56 -0700

    superbalanced complete

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -13,8 +13,30 @@ function BinaryTree(value) {
   this.right = null;
 }
 
-function superbalanced(tree) {
+function findHeight(tree) {
+  if (!tree) return 0;
+  let left = findHeight(tree.left);
+  let right = findHeight(tree.right);
+  if (left > right) {
+    return left + 1;
+  } else {
+    return right + 1;
+  }
+}
 
+function superbalanced(tree) {
+  if (!tree) return true;
+  return Math.abs(findHeight(tree.left) - findHeight(tree.right)) < 2 && superbalanced(tree.left) && superbalanced(tree.right);
 }
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
+
+
+const myTree = new BinaryTree(6);
+myTree.left = new BinaryTree(3);
+myTree.left.right = new BinaryTree(4);
+myTree.right = new BinaryTree(8);
+myTree.left.right.right = new BinaryTree(5);
+
+console.log(findHeight(myTree)); // 3
+console.log(superbalanced(myTree)); // false

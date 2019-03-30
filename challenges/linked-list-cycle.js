eb44da5b29c90fdeed45b6b51ba0bc175ee37514
tang <eytang8@gmail.com>
2019-03-30 09:25:28 -0700

    set solution for cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -33,7 +33,25 @@ var Node = function(value) {
 }
 
 function hasCycle(head) {
-
+  const visited = new Set()
+  function recurse(node){
+    if (node.next === null) return false;
+    if (visited.has(node)) return true;
+    visited.add(node)
+    return recurse(node.next);
+  }
+  return recurse(head);
 }
+
+/*
+var node1 = new Node('1');
+var node2 = node1.next = new Node('2');
+var node3 = node2.next = new Node('3');
+var node4 = node3.next = new Node('4');
+var node5 = node4.next = new Node('5');
+console.log(hasCycle(node1), '=> false')
+node5.next = node2;
+console.log(hasCycle(node1), '=> true')
+*/
 
 module.exports = {Node: Node, hasCycle: hasCycle}

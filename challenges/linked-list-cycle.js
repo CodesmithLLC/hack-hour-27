a4d83befc05c3383d72e72e9c406f8fb637b1da3
Sterling Deng <sterlingdeng@gmail.com>
2019-03-31 21:26:41 -0700

    ll cycle

--- challenges/linked-list-cycle.js
+++ challenges/linked-list-cycle.js
@@ -30,10 +30,37 @@
 var Node = function(value) {
   this.value = value;
   this.next = null;
+};
+
+function hasCycleNaive(head) {
+  return helper(head);
+}
+function helper(node, memo = new Set()) {
+  if (memo.has(node)) return true;
+  if (!node) return false;
+  memo.add(node);
+  return helper(node.next, memo);
 }
 
 function hasCycle(head) {
-
+  function hasCycleHelper(node) {
+    if (node.hasVisited) return true;
+    if (!node.hasVisited) {
+      node.hasVisited = true;
+    }
+    return helper(node.next);
+  }
+  return hasCycleHelper(head);
 }
 
-module.exports = {Node: Node, hasCycle: hasCycle}
+// var node1 = new Node('1');
+// var node2 = (node1.next = new Node('2'));
+// var node3 = (node2.next = new Node('3'));
+// var node4 = (node3.next = new Node('4'));
+// var node5 = (node4.next = new Node('5'));
+// hasCycle(node1); // => false
+// node5.next = node2;
+// hasCycle(node1); // => true
+// console.log(hasCycle(node2));
+
+module.exports = { Node: Node, hasCycle: hasCycle };

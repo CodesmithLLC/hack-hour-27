1334f502d0689e4374c88b710d4131ee0c6eb745
mlu456 <michaellu213@gmail.com>
2019-04-01 09:31:59 -0700

    mult 3 or 5

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -6,18 +6,16 @@
 // below 1000 and return that sum.
 
 function sumMultiples3Or5Below1000() {
-  let sum = 0;
-
-  return sum;
+  let num = 999;
+  return (Math.floor(num/3)*(3*1 + 3*Math.floor(num/3))/2) + (Math.floor(num/5)*(5*1 + 5*Math.floor(num/5))/2) - (Math.floor(num/15)*(15*1 + 15*Math.floor(num/15))/2)
 }
-
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
-  let sum = 0;
-
-  return sum;
+  return (Math.floor((z-1)/x)*(x + x*Math.floor((z-1)/x))/2)
+    + (Math.floor((z-1)/y)*(y + y*Math.floor((z-1)/y))/2)
+    - (Math.floor((z-1)/(x*y))*((x*y) + (x*y)*Math.floor((z-1)/(x*y)))/2)
 }
 
 const objectToExport = {

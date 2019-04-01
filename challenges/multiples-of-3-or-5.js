9705bb1cd37bff8ba9419ed36f19711005c94de4
Sterling Deng <sterlingdeng@gmail.com>
2019-04-01 10:22:55 -0700

    pr

--- challenges/multiples-of-3-or-5.js
+++ challenges/multiples-of-3-or-5.js
@@ -8,21 +8,60 @@
 function sumMultiples3Or5Below1000() {
   let sum = 0;
 
+  const threes = floor(1000 / 3);
+  const fives = floor(1000 + 1 / 5);
+  const fifteens = floor(1000 / 15);
+
+  sum =
+    3 * ((threes * (threes + 1)) / 2) +
+    5 * ((fives * (fives + 1)) / 2) -
+    15 * ((fifteens * (fifteens + 1)) / 2);
+
   return sum;
 }
-
 
 // extension make it dynamic function that takes input x,y,z
 // and returns the sum of multiples of x and y below z
 function sumMultiplesXOrYBelowZ(x, y, z) {
   let sum = 0;
 
+  let x_count;
+  let y_count;
+  let xy_count;
+
+  if (z % x === 0) {
+    x_count = Math.floor((z - 1) / x);
+  } else {
+    x_count = Math.floor(z / x);
+  }
+
+  if (z % y === 0) {
+    y_count = Math.floor((z - 1) / y);
+  } else {
+    y_count = Math.floor(z / y);
+  }
+
+  const xy = x * y;
+
+  if (z % xy === 0) {
+    xy_count = Math.floor((z - 1) / xy);
+  } else {
+    xy_count = Math.floor(z / xy);
+  }
+
+  sum =
+    x * ((x_count * (x_count + 1)) / 2) +
+    y * ((y_count * (y_count + 1)) / 2) -
+    xy * ((xy_count * (xy_count + 1)) / 2);
+
   return sum;
 }
 
 const objectToExport = {
   sumMultiples3Or5Below1000,
-  sumMultiplesXOrYBelowZ,
+  sumMultiplesXOrYBelowZ
 };
+
+console.log(sumMultiplesXOrYBelowZ(3, 5, 16));
 
 module.exports = objectToExport;

d431effddaf4c8c433d8da47a16c9df6d04b6a4f
Augustine Kim <ajk830@gmail.com>
2019-04-02 09:34:53 -0700

    max-subarray complete

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,30 @@
  */
 
 function maxSubarray(arr) {
-
+  let sum = arr[0];
+  let min = arr[0];
+  let maxSum = sum;
+  let recount = false;
+  for (let i = 1; i < arr.length; i++) {
+    if (recount) {
+      sum = arr[i];
+      min = arr[i];
+      maxSum = Math.max(maxSum, sum);
+      recount = false;
+    } else {
+      if (sum + arr[i] < min) {
+        recount = true;
+      } else {
+        sum += arr[i];
+        maxSum = Math.max(maxSum, sum);
+      }
+    }
+    // console.log(`i: ${i}, sum: ${sum}, min: ${min}, maxSum: ${maxSum}`);
+  }
+  return Math.max(maxSum, sum);
 }
 
 module.exports = maxSubarray;
+
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5])); // -> 18 from [3, 10, -4, 7, 2]
+// console.log(maxSubarray([15,20,-5,10])); //  -> 40

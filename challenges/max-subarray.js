f88918848f8324fb1700cd7fe9e981d0eed48894
Chris Air <sdchrisn@gmail.com>
2019-04-02 09:26:54 -0700

    First attempt at hack hour, will try to find a more optimized solution

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,25 @@
  */
 
 function maxSubarray(arr) {
+    let max = 0;
+    let len = arr.length;
+    for(let i = 0; i < len; i++){
 
+        for(let j = len-1; j > i; j--){
+
+            let sum = 0;
+            for(let k = i; k <= j ; k++)
+                sum += arr[k];
+
+            if(sum > max)
+                max = sum;
+        }
+    }
+    return max;
 }
 
+// console.log(maxSubarray([1, -2, 3, 10, -4, 7, 2, -5]));
+// console.log(maxSubarray([15,20,-5,10]));
+
 module.exports = maxSubarray;
+

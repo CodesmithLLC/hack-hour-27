0055e2b2a6f01f4cf7a75f2f996e8c08e0343a03
Sterling Deng <sterlingdeng@gmail.com>
2019-04-03 09:46:19 -0700

    i don't like anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -7,13 +7,47 @@
  */
 
 /**
-  * example:
-  * var result = anagrams('abc');
-  * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
-  */
+ * example:
+ * var result = anagrams('abc');
+ * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+ */
 
 function anagrams(string) {
+  const strArr = Array.from(string);
+  const anagram = [];
 
+  const truthTable = Array(string.length).fill(false);
+
+  for (let i = 0; i < strArr.length; i += 1) {
+    const char = strArr[i];
+    truthTable[i] = true;
+    recurser(strArr, char, truthTable, anagram);
+    truthTable[i] = false;
+  }
+
+  return anagram;
 }
+
+function recurser(strArr, letter, truthTable, anagram, buildUp = []) {
+  buildUp.push(letter);
+  // console.log(buildUp);
+  if (buildUp.length === strArr.length) {
+    anagram.push(buildUp.join(''));
+    buildUp.pop();
+    return;
+  }
+
+  for (let i = 0; i < strArr.length; i += 1) {
+    const char = strArr[i];
+    if (!truthTable[i]) {
+      truthTable[i] = true;
+      recurser(strArr, char, truthTable, anagram, buildUp);
+      truthTable[i] = false;
+    }
+  }
+  buildUp.pop();
+}
+
+// a
 
 module.exports = anagrams;

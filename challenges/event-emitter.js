92f88fb943d3fa6e022c126fcd3f70d0a7e642d3
Augustine Kim <ajk830@gmail.com>
2019-04-05 09:18:08 -0700

    event-emitter complete

--- challenges/event-emitter.js
+++ challenges/event-emitter.js
@@ -22,15 +22,26 @@
  */
 
 function EventEmitter() {
-
+  this.funcs = {};
 }
 
 EventEmitter.prototype.on = function(funcName, func) {
-
+  this.funcs.funcName = func;
 };
 
 EventEmitter.prototype.trigger = function(funcName, ...args) {
-
+  this.funcs.funcName(...args);
 };
 
 module.exports = EventEmitter;
+
+// var instance = new EventEmitter();
+// var counter = 0;
+// instance.on('increment', function() {
+//   counter++;
+// }); // counter should be 0
+// console.log('counte expect 0:', counter)
+// instance.trigger('increment'); // counter should be 1
+// console.log('counter expect 1:', counter);
+// instance.trigger('increment'); // counter should be 2
+// console.log('counter expect 2:', counter);

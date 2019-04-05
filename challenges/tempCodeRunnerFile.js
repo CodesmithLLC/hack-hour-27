19e3d98c7daa5ad863be4e40f5225e63869b73b8
Ray Yao <rocaray51@gmail.com>
2019-04-05 09:29:50 -0700

    event-emitter fix

--- challenges/tempCodeRunnerFile.js
+++ challenges/tempCodeRunnerFile.js
@@ -1,5 +1,8 @@
-console.log(countTwos(1)); // -> 0
-// console.log(countTwos(3)); // -> 1
-// console.log(countTwos(13)); // -> 2
-// console.log(countTwos(1000)); // -> 300
-// console.log(countTwos(11420)); // -> 4483
+var instance = new EventEmitter();
+var counter = 0;
+instance.on('increment', function() {
+  counter++;
+}); // counter should be 0
+instance.trigger('increment'); // counter should be 1
+instance.trigger('increment'); // counter should be 2
+console.log('counter after triggers', counter)

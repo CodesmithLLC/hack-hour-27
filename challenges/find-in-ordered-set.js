a4098bbe79c86aa6f8a85c74717a2b6636fda340
Augustine Kim <ajk830@gmail.com>
2019-04-06 09:23:36 -0700

    find-in-ordered-set complete with binary search

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,21 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
-
+  if (arr.length === 1) {
+    return target === arr[0];
+  }
+  const middle = Math.floor(arr.length/2);
+  if (target >= arr[middle]) {
+    return findInOrderedSet(arr.slice(middle), target);
+  } else {
+    return findInOrderedSet(arr.slice(0, middle), target);
+  }
 }
 
 
 module.exports = findInOrderedSet;
+
+
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log(findInOrderedSet(nums, 4)); // -> true
+// console.log(findInOrderedSet(nums, 2)); // -> false

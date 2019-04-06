cc6627bcdc9e41f62d0a917f5339d65d4259cbf0
tang <eytang8@gmail.com>
2019-04-06 09:40:50 -0700

    finished find-in-ordered-set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -10,9 +10,23 @@ findInOrderedSet(nums, 2);  -> false
  */
 
 
-function findInOrderedSet(arr, target) {
-
+// pick halfway point. is it higher or lower? 
+// do it again
+function findInOrderedSet(arr, target, guess = Math.floor(arr.length/2), prevGuess = null){
+  if (guess === prevGuess) return false;
+  if (arr[guess] === target) return true;
+  if (arr.length === 0) return false;
+  if (target > arr[guess]){
+    return findInOrderedSet(arr.slice(guess), target, Math.floor((guess+arr.length)/2), guess);
+  } else {
+    return findInOrderedSet(arr.slice(0, guess), target, Math.floor(guess/2), guess);
+  }
 }
 
+/*
+var nums = [1, 4, 6, 7, 9, 17, 45]
+console.log(findInOrderedSet(nums, 4), ' -> true');
+console.log(findInOrderedSet(nums, 2), ' -> false');
+*/
 
 module.exports = findInOrderedSet;

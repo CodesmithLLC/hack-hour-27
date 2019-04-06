d6bd98e8f5f2cbb9daddb5fc73a261859fc9479d
Ed <15ryane@gmail.com>
2019-04-06 09:20:28 -0700

    find-in-ordered-set v1

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,25 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
+// O(log(n)) solution
 
 function findInOrderedSet(arr, target) {
+  if(arr.length === 1) return arr[0] === target ? true : false;
+  if(arr[Math.floor(arr.length/2)] === target) return true
+  if(arr[Math.floor(arr.length/2)] > target) {
+    return findInOrderedSet(arr.slice(0, arr.length/2), target)
+  }
+  if(arr[Math.floor(arr.length/2)] < target) {
+    return findInOrderedSet(arr.slice(arr.length/2, arr.length), target)    
+  }
 
 }
+
+// tests
+
+// var nums = [1, 4, 6, 7, 9, 17, 45]
+// console.log(findInOrderedSet(nums, 9) );  //-> true
+// console.log(findInOrderedSet(nums, 2) );  //-> false
 
 
 module.exports = findInOrderedSet;

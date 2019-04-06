45d8b0720f4c914647ec989e6d3577ff4886b55f
Sterling Deng <sterlingdeng@gmail.com>
2019-04-06 09:24:31 -0700

    fios

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -9,10 +9,36 @@ findInOrderedSet(nums, 2);  -> false
 
  */
 
-
-function findInOrderedSet(arr, target) {
-
+// time complexity should be log n since we halving the searchable space every call.
+// search space / 2^n = log(n)
+function getFirstHalf(arr) {
+  return arr.slice(0, Math.floor(arr.length / 2));
 }
 
+function getSecondHalf(arr) {
+  return arr.slice(Math.floor(arr.length / 2));
+}
+
+function findInOrderedSet(arr, target) {
+  const mid = arr[Math.floor(arr.length / 2)];
+
+  if (mid === target) {
+    return true;
+  }
+
+  if (arr.length <= 1) {
+    return false;
+  }
+
+  if (target < mid) {
+    return findInOrderedSet(getFirstHalf(arr), target);
+  } else {
+    return findInOrderedSet(getSecondHalf(arr), target);
+  }
+}
+
+// console.log("test first half", getFirstHalf([1, 3, 6, 7, 9, 17, 45]));
+// console.log("test second half", getSecondHalf([1, 3, 6, 7, 9, 17, 45]));
+// console.log(findInOrderedSet([1, 3, 6, 7, 9, 17, 45], 6));
 
 module.exports = findInOrderedSet;

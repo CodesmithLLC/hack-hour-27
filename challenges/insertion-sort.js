4d8bac3342949f457e464de0cba265ebca90f3ed
mlu456 <michaellu213@gmail.com>
2019-04-08 09:13:13 -0700

    insert sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,14 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[i]) {
+        [array[j], array[i]] = [array[i], array[j]]
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;

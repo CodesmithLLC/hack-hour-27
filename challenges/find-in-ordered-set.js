53f254272bfce9d2849e74b27150fd877511ec91
Ray Yao <rocaray51@gmail.com>
2019-04-08 09:05:47 -0700

    Merge branch 'master' of https://github.com/CodesmithLLC/hack-hour-27

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -14,14 +14,18 @@ function findInOrderedSet(arr, target) {
   // O(n)
   //return arr.includes(target)
 
+  //O(n/2)??? prob same as O(n)
   //2 pointers: 1 at beginning, 1 at end, and move both toward each other
   let i = 0;
   while (i <= Math.floor(arr.length / 2)) {
     if (arr[i] === target) return true;
-    if (arr[arr.length - 1 - 1] === target) return true;
+    if (arr[arr.length - 1 - i] === target) return true;
     i++;
   }
   return false;
+
+  //better way: search using binary search tree
+  
 }
 
 var nums = [1, 4, 6, 7, 9, 17, 45]

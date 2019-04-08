20e8d4880c1e750fe79241c5230ba533ae651543
Chris Air <sdchrisn@gmail.com>
2019-04-08 11:18:13 -0700

    completed insertion sort algorithm

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,21 @@
 
 
 function insertionSort(array) {
+    if(!array.length) return [];
 
+    for(let i = 1; i < array.length; i++){
+        for(let j = i; j > 0; j--){
+            if(array[j-1] > array[j]){
+                let temp = array[j];
+                array[j] = array[j-1];
+                array[j-1] = temp;
+            }
+            else{ break; }
+        }
+    }
+    return array;
 }
+
+// console.log(insertionSort([40,13,20,4]));
 
 module.exports = insertionSort;

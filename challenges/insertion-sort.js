6e39d73b15b1b15be1480b0466d796af0e504bb7
Augustine Kim <ajk830@gmail.com>
2019-04-08 09:22:21 -0700

    insertion-sort complete

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i ++) {
+    for (let j = i; array[j] < array[j-1]; j--) {
+     [array[j-1], array[j]] = [array[j], array[j-1]];
+    }
+  }
+  return array;
 }
 
 module.exports = insertionSort;
+
+// const arr = [4,7,1,10,40,35,20];
+// console.log(insertionSort(arr));

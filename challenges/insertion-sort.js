25292bdde7669b5f8c79b4c5d10a91b6389339a1
Parker Allen <parkerjallen1@gmail.com>
2019-04-09 09:29:56 -0700

    unique number

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,24 @@
 
 
 function insertionSort(array) {
-
-}
+    for (i = 0; i < array.length; i++) {
+  
+      if (array[i] > array[i + 1]) {
+        [array[i], array[i + 1]] = [array[i + 1], array[i]]
+        for (let j = i; j > 0; j--) {
+          if (array[j] < array[j - 1]) {
+            [array[j], array[j - 1]] = [array[j - 1], array[j]]
+          } else {
+            break;
+          }
+        }
+      }
+    }
+    return array;
+  
+  }
 
 module.exports = insertionSort;
+const arr = [10, 9, 8, 7, 6, 11, 23, 5, 4, 1, 3]
+insertionSort(arr)
+console.log(arr)
--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -9,8 +9,22 @@
  * Complete the challenge in O(1) space
  *
  */
-function uniqueNumber(array) {
+// function uniqueNumber(array) {
+//  const map = array.reduce((acc, val)=>{
+//   acc[val] = acc[val] + 1 || 1;
+//   return acc;
+//  }, {})
 
+//  for (let [k, v] of Object.entries(map)) {
+//    if (v === 1) return k;
+//  }
+// }
+
+
+function uniqueNumber(array) {
+  return array.reduce((a,b)=>a^b)
 }
 
 module.exports = uniqueNumber;
+
+console.log(uniqueNumber([1,2,1,3,3]))

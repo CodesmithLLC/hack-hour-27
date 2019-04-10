d9fbb592e1a9e96ad8c3f4a9af96e20bc6504781
Augustine Kim <ajk830@gmail.com>
2019-04-10 09:36:26 -0700

    get-all-products complete

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,17 @@
  */
 
 function getAllProducts(array) {
-
+  let output = [];
+  for (let i = 0; i < array.length; i++) {
+    let product = 1;
+    for (let j = 0; j < array.length; j++) {
+      if (j !== i) product *= array[j];
+    }
+    output.push(product);
+  }
+  return output;
 }
 
 module.exports = getAllProducts;
+
+console.log(getAllProducts([1, 7, 3, 4])); // -> [84, 12, 28, 21]

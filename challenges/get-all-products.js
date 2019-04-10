9a45ec67c10cf670373d199703653660e9d618ff
Sterling Deng <sterlingdeng@gmail.com>
2019-04-10 09:31:22 -0700

    gap

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,43 @@
  */
 
 function getAllProducts(array) {
+  const startFromLeft = Array(array.length).fill(undefined);
+  startFromLeft[0] = array[0];
+  for (let i = 1; i < array.length; i += 1) {
+    startFromLeft[i] = array[i] * startFromLeft[i - 1];
+  }
 
+  const startFromRight = Array(array.length).fill(undefined);
+  startFromRight[array.length - 1] = array[array.length - 1];
+  for (let i = array.length - 2; i >= 0; i -= 1) {
+    startFromRight[i] = array[i] * startFromRight[i + 1];
+  }
+
+  const gotAllProducts = Array(array.length).fill(undefined);
+  gotAllProducts[0] = startFromRight[1];
+  gotAllProducts[array.length - 1] = startFromLeft[array.length - 2];
+
+  for (let i = 1; i < array.length - 1; i += 1) {
+    gotAllProducts[i] = startFromLeft[i - 1] * startFromRight[i + 1];
+  }
+
+  return gotAllProducts;
 }
+
+function getAllProducts_naive(array) {
+  const gotAllProducts = Array(array.length).fill(undefined);
+
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = 0; j < array.length; j += 1) {
+      if (j !== i) {
+        gotAllProducts[i] = gotAllProducts[i] ? gotAllProducts[i] * array[j] : array[j];
+      }
+    }
+  }
+
+  return gotAllProducts;
+}
+
+//console.log(getAllProducts_naive([1, 7, 3, 4]));
 
 module.exports = getAllProducts;

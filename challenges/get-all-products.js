abf18288d55ea0bf6f6b8d727742c5e613729eff
Chris Air <sdchrisn@gmail.com>
2019-04-10 09:41:35 -0700

    ready for eval, will try to refactor better

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -6,11 +6,25 @@
  * this is done via:
  * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
  *
- * do not use division, becuase zero might be in the array and you cannot divide by zero
+ * do not use division, because zero might be in the array and you cannot divide by zero
  */
 
 function getAllProducts(array) {
+    const arr = new Array(array.length);
+    for(let i = 0; i < arr.length; i++) arr[i] = 1;
 
+    console.log('first loop: ', arr);
+    for(let i = 0; i < arr.length; i++){
+        let count = 0;
+        while(count < arr.length-1){
+            let first = arr.shift();
+            arr.push(first * array[i]);
+            count++;
+        }
+    }
+    return arr;
 }
+
+// console.log(getAllProducts([1, 7, 3, 4])); // ->  [84, 12, 28, 21]
 
 module.exports = getAllProducts;

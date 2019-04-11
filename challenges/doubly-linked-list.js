7fde3496bf970807b6204e949efdd88f89462764
Shane Taylor <shaneallantaylor@gmail.com>
2019-04-11 09:49:21 -0700

    I think i flipped head and tail in my mind

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,69 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+
+// I think this works. Maybe.
+LinkedList.prototype.add = function (val) {
+  const newNode = new Node(val);
+  // so here we are, with a beautiful Linked List
+  // It might be empty (OH NO)
+  // but it might also have a bunch of shit in it (oh damn)
+  // so let's check for that
+  if (this.head === null) {
+    // omg our linked list is empty (so sad)
+    // so lets add the head
+    this.head = newNode;
+    this.tail = newNode;
+
+  } else {
+    this.head.next = newNode;
+    newNode.prev = this.head;
+    this.head = newNode;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  // again, our linkedList could be EMPTY or FULL
+
+  // if empty
+  if (this.head !== null && this.tail !== null) {
+    console.log('we dont have an empty LL');
+    // lets travel forward;
+    let pointer = this.tail;
+    while (pointer !== null) {
+      if (pointer.val === val) {
+        console.log('WE HIT THE VALUE!!!');
+        // ok so we are at the value we want to remove
+        // we probably need to do different shit based on if we are at the head or tail
+        if (pointer.prev === null) {
+          console.log('we are trying to remove the tail');
+          pointer.next.prev = null;
+          this.tail = pointer.next;
+        } else if (pointer.next === null) {
+          console.log('we are trying to remove the head')
+          pointer.prev.next = null;
+          this.head = pointer.prev;
+        } else {
+          console.log('we are trying to remove anything other than the head or tail');
+          // so we are removing something in the middle
+          const nextNode = pointer.next;
+          const prevNode = pointer.prev;
+
+          prevNode.next = nextNode;
+          nextNode.prev = prevNode;
+          return this.head;
+        }
+
+      }
+      pointer = pointer.next;
+    }
+  }
+
 };
+
+
 
 module.exports = LinkedList;

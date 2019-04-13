0a56e74838e4778096522afd59761b69a41c2c23
Ed <15ryane@gmail.com>
2019-04-13 09:21:50 -0700

    bubble sort v1

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -2,10 +2,21 @@
 
 // https://en.wikipedia.org/wiki/Bubble_sort
 
-
-
 function bubbleSort(array) {
-
+  for(let i=0; i<array.length-1; i++){
+    for(let j=0; j<array.length-i; j++) {
+      if(array[j] > array[j+1]) {
+        [array[j+1], array[j]] = [array[j], array[j+1]];
+      }
+    }
+  }
 }
+
+// tests
+
+// const a = [1,5,3,3,8,9,2,4]
+// bubbleSort(a)
+// console.log(a)
+
 
 module.exports = bubbleSort;

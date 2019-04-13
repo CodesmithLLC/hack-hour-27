1ef50c1807b2ed2cd0a04f5471112ed131a668d2
tang <eytang8@gmail.com>
2019-04-13 09:14:30 -0700

    fixed condition

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -8,15 +8,17 @@ function bubbleSort(array) {
 
   let sorted = false;
   while (!sorted){
+    sorted = true
     for (let i = 0; i < array.length; i ++){
-      sorted = true;
       if (array[i+1] && (array[i+1] < array[i])){
         [array[i], array[i+1]] = [array[i+1], array[i]]
-        sorted = false;
+        sorted = false
       }
     }
   }
   return array;
 }
+
+console.log(bubbleSort([5,3,8,1]))
 
 module.exports = bubbleSort;

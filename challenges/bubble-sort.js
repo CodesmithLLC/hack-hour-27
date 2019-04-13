19f853be03e0179484c094c21e3971b33a384d6e
tang <eytang8@gmail.com>
2019-04-13 09:09:07 -0700

    champagne sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -6,6 +6,17 @@
 
 function bubbleSort(array) {
 
+  let sorted = false;
+  while (!sorted){
+    for (let i = 0; i < array.length; i ++){
+      sorted = true;
+      if (array[i+1] && (array[i+1] < array[i])){
+        [array[i], array[i+1]] = [array[i+1], array[i]]
+        sorted = false;
+      }
+    }
+  }
+  return array;
 }
 
 module.exports = bubbleSort;

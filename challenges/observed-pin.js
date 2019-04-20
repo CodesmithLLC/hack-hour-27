5e7754a0d6657ae2fd98e7cfe076c25e42b289a0
tang <eytang8@gmail.com>
2019-04-20 10:35:58 -0700

    thanks nishaaaaaa

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -41,9 +41,33 @@ expectations = {
 
 
 
+const possibilities = [
+  ['0', '8'],
+  ['1', '2', '4'],
+  ['2', '1','3','5'],
+  ['3', '2','6'],
+  ['4', '1','5','7'],
+  ['5', '2','4','6','8'],
+  ['6', '3','5','9'],
+  ['7', '4','8'],
+  ['8', '7','5','9','0'],
+  ['9', '6','8']
+]
 
 function getPINs(observed) {
-
+  const nums = observed.split('');
+  const comboTree = nums.map((item)=>{
+    return possibilities[item];
+  })
+  return comboTree.reduce((acc, cur)=>{
+    const output = [];
+    for (let i = 0; i < acc.length; i++){
+      for (let j = 0; j < cur.length; j++){
+        output.push(acc[i]+cur[i]);
+      }
+    }
+    return output;
+  })
 }
 
 

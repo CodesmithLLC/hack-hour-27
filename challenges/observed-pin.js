8aa751892a51a7e8e31a7a75c571a0ef21802f01
Augustine Kim <ajk830@gmail.com>
2019-04-20 09:45:17 -0700

    observed-pin complete

--- challenges/observed-pin.js
+++ challenges/observed-pin.js
@@ -43,8 +43,31 @@ expectations = {
 
 
 function getPINs(observed) {
+  const possibles = {
+    1: ['1','2','4'],
+    2: ['2','5'],
+    3: ['2','3','6'],
+    4: ['1','4','5','7'],
+    5: ['2','4','5','6','8'],
+    6: ['3','5','6','9'],
+    7: ['4','7','8'],
+    8: ['5','7','8','9','0'],
+    9: ['6','8','9'],
+    0: ['8','0']
+  }
 
+  if (observed.length === 1) return possibles[observed];
+
+  return getPINs(observed.slice(1)).reduce((acc, cur) => {
+    possibles[cur].forEach(e=>{
+      acc.push(e+cur);
+    })
+    return acc;
+  }, [])
 }
 
 
 module.exports = getPINs
+
+// console.log(getPINs('8'));
+// console.log(getPINs('11'));

8cc3ab23d40fc93e99345cad313ebc6f940de14b
Shane Taylor <shaneallantaylor@gmail.com>
2019-04-22 09:59:48 -0700

    count stairs n squared

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,7 +14,35 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
+
+
 function countStairs(n) {
+
+  const baseWay = [];
+  for (let i = 0; i < n; i += 1) {
+    baseWay.push(1);
+  }
+
+  const solutions = [baseWay];
+
+  for (let i = 0; i < solutions.length; i += 1) {
+    console.log('SOLUTIONS AT I IS', solutions[i]);
+    wayFinder(solutions[i], solutions);
+  }
+
+  function wayFinder(arr, target) {
+    for (let i = 0; i < arr.length; i += 1) {
+      let newWay = arr.slice(0);
+      if (arr[i] === 1 && arr[i + 1] === 1) {
+        newWay.splice(i, 2, 2);
+        target.push(newWay);
+      }
+    }
+  }
+
+  const uniqueSolutions = Array.from(new Set(solutions.map(JSON.stringify)), JSON.parse)
+
+  return uniqueSolutions.length;
 
 }
 

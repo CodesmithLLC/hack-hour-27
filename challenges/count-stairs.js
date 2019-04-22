63a000d02e9a3d158ce4054e83447f09317473c3
tang <eytang8@gmail.com>
2019-04-22 12:55:25 -0700

    finished factorial count stairs

--- challenges/count-stairs.js
+++ challenges/count-stairs.js
@@ -14,8 +14,29 @@
  * That is a total of 8 different ways to take 5 steps, given that you can take 1 or 2 steps at a time.
  */
 
-function countStairs(n) {
+const factorial = (num, tot = 1)=>{
+  if (num === 0){
+    return 1;
+  }
+  else if (num === 1){ 
+    return tot
+  }else{
+    return factorial(num-1, tot * num);
+  }
+}
 
+const getCombo = (n, steps)=>{
+  const twos = steps-n;
+  const ones = steps-(2*twos);
+  return (factorial(n))/(factorial(ones)*factorial(twos))
+}
+
+function countStairs(sum) {
+  let combos = 0;
+  for(let i = sum; i >= sum/2; i--){
+    combos += getCombo(i, sum)
+  }
+  return combos
 }
 
 module.exports = countStairs;

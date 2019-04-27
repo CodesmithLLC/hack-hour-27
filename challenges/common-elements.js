3aab305713ef6aaf8ed5bb6c4d0c7f2714856e42
Augustine Kim <ajk830@gmail.com>
2019-04-27 09:24:19 -0700

    common-elements complete

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -13,7 +13,27 @@
 // if there are no common numbers or strings return the string "Nothing in Common!"
 
 function commonElements(array1, array2, array3, array4){
-
+  const set = new Set(array1);
+  const set2 = new Set();
+  for (val of array2) {
+    if (set.has(val)) set2.add(val);
+  }
+  const set3 = new Set();
+  for (val of array3) {
+    if (set2.has(val)) set3.add(val);
+  }
+  const output = []
+  for (val of array4) {
+    if (set3.has(val) && !output.includes(val)) output.push(val);
+  }
+  if (!output.length) return "Nothing in Common!";
+  else return output;
 }
 
 module.exports = commonElements;
+
+// var array1 = [1,4,6,7,'ferret',12,12,99,2000,'dog','dog',99,1000];
+// var array2  = [15,9,9,'ferret',9,26,12,12,'dog'];
+// var array3 = [23,12,12,77,'ferret',9,88,100,'dog'];
+// var array4 = ['ferret',12,12,45,9,66,77,78,2000];
+// console.log(commonElements(array1, array2, array3, array4));

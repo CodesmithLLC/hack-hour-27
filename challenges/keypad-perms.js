be8c71d05404966ac1676ec8f3c98f0046c604a2
justnoah <no@hlee.me>
2019-06-06 01:46:24 -0700

    Added keypad-perms.js; challenges.

---
+++ challenges/keypad-perms.js
@@ -0,0 +1,7 @@
+// Each number key on a standard phone keypad has a set of Latin letters written on it as well:  
+
+// http://en.wikipedia.org/wiki/File: Telephone-keypad2.svg 
+
+// Write a function that takes up to four digits of a phone number, and returns a list of all of the words that can be written on the phone with that number. (You should return all permutations, not only English words.)
+
+// * Tips:  - Phone numbers are strings. (A phone number can start with a zero.)  - The digits 0 and 1 do not have letters associated with them, so they should be left as numbers.  - Don't return every combination of those digits in any order, just the order given. 
